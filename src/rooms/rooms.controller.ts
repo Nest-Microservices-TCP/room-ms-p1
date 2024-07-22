cb5cbfd6ca01d008e43bb3af07baa5c5ccb225d2
JosueEC <josueev.cruz@gmail.com>
2024-07-21 19:40:45 -0600

    [FEAT](rooms): Add the basic CRUD methods to the controller

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -4,4 +4,14 @@ import { RoomsService } from './rooms.service';
 @Controller()
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
+
+  create() {}
+
+  findOne() {}
+
+  findAll() {}
+
+  update() {}
+
+  delete() {}
 }
