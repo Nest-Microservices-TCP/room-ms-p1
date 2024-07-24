2df0bd08a4f7896173be0162ba3c675e969a850d
JosueEC <josueev.cruz@gmail.com>
2024-07-23 22:14:25 -0600

    [FEAT](config): Add the envs validation for database connection

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,12 +4,24 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   HOST: string;
+
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_NAME: string;
 }
 
 const envSchema = joi
   .object({
     PORT: joi.number().required(),
     HOST: joi.string().required(),
+
+    DB_HOST: joi.string().required(),
+    DB_PORT: joi.number().required(),
+    DB_USERNAME: joi.string().required(),
+    DB_PASSWORD: joi.string().required(),
+    DB_NAME: joi.string().required(),
   })
   .unknown(true);
 
@@ -24,4 +36,10 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   host: envVars.HOST,
+
+  dbHost: envVars.DB_HOST,
+  dbPort: envVars.DB_PORT,
+  dbUsername: envVars.DB_USERNAME,
+  dbPassword: envVars.DB_PASSWORD,
+  dbName: envVars.DB_NAME,
 };
