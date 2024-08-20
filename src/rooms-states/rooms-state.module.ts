35d52f74c74e1ba79899dafc0e56f8b28e3a0d5e
JosueEC <josueev.cruz@gmail.com>
2024-08-19 19:58:19 -0600

    [FEAT](rooms-state.module): Inject the rooms-state-repository and unit-of-work service

--- src/rooms-states/rooms-state.module.ts
+++ src/rooms-states/rooms-state.module.ts
@@ -3,10 +3,12 @@ import { RoomsStatesController } from './rooms-state-controller';
 import { RoomsStatesService } from './rooms-state.service';
 import { RoomStateEntity } from './entities/room-state.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomsStatesRepository } from './repositories/rooms-state.repository';
+import { UnitOfWork } from 'src/common/unit-of-work/unit-of-work.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RoomStateEntity])],
   controllers: [RoomsStatesController],
-  providers: [RoomsStatesService],
+  providers: [UnitOfWork, RoomsStatesService, RoomsStatesRepository],
 })
 export class RoomsStatesModule {}
