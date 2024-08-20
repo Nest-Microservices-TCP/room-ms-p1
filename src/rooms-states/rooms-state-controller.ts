91a72c277bb236ffc94d394fdb42f9a28681022f
JosueEC <josueev.cruz@gmail.com>
2024-08-19 19:57:33 -0600

    [FEAT](rooms-state.service): Add the save method and use the unitForWork service

--- src/rooms-states/rooms-state-controller.ts
+++ src/rooms-states/rooms-state-controller.ts
@@ -1,4 +1,13 @@
 import { Controller } from '@nestjs/common';
+import { RoomsStatesService } from './rooms-state.service';
+import { CreateRoomStateDto } from './dto/create-room-state.dto';
+import { RoomStateEntity } from './entities/room-state.entity';
 
 @Controller()
-export class RoomsStatesController {}
+export class RoomsStatesController {
+  constructor(private readonly roomsStatesService: RoomsStatesService) {}
+
+  async save(request: CreateRoomStateDto): Promise<RoomStateEntity> {
+    return this.roomsStatesService.save(request);
+  }
+}
--- src/rooms-states/rooms-state.service.ts
+++ src/rooms-states/rooms-state.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { UnitOfWork } from '../common/unit-of-work/unit-of-work.service';
+import { RoomStateEntity } from './entities/room-state.entity';
+import { CreateRoomStateDto } from './dto/create-room-state.dto';
 
 @Injectable()
-export class RoomsStatesService {}
+export class RoomsStatesService {
+  constructor(private readonly unitOfWork: UnitOfWork) {}
+
+  async save(request: CreateRoomStateDto): Promise<RoomStateEntity> {
+    let newRoomState: RoomStateEntity;
+
+    try {
+      await this.unitOfWork.complete(async () => {
+        const roomStatesRepository = this.unitOfWork.getRoomsStatesRepository();
+
+        newRoomState = await roomStatesRepository.save(request);
+      });
+
+      return newRoomState;
+    } catch (error) {
+      throw new Error(
+        `Error al guardar el estado de habitación: ${error.message}`,
+      );
+    }
+  }
+}
