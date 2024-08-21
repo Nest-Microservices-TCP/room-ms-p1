c56bcd811c44293b58c1011c8f8b2aa47de3de61
JosueEC <josueev.cruz@gmail.com>
2024-08-20 19:35:24 -0600

    [FEAT](rooms-state.controller): Add the message pattern to the save method

--- src/rooms-states/rooms-state-controller.ts
+++ src/rooms-states/rooms-state-controller.ts
@@ -2,12 +2,14 @@ import { Controller } from '@nestjs/common';
 import { RoomsStatesService } from './rooms-state.service';
 import { CreateRoomStateDto } from './dto/create-room-state.dto';
 import { RoomStateEntity } from './entities/room-state.entity';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class RoomsStatesController {
   constructor(private readonly roomsStatesService: RoomsStatesService) {}
 
-  async save(request: CreateRoomStateDto): Promise<RoomStateEntity> {
+  @MessagePattern({ cmd: 'save_room_state' })
+  async save(@Payload() request: CreateRoomStateDto): Promise<RoomStateEntity> {
     return this.roomsStatesService.save(request);
   }
 }
