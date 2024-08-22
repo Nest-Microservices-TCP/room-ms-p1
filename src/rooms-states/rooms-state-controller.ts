e9db77cb4b850250ca386ccf95d4853f32d247d2
JosueEC <josueev.cruz@gmail.com>
2024-08-21 20:13:43 -0600

    [REFACTOR](rooms-states.controller): Modify the command for save method

--- src/rooms-states/rooms-state-controller.ts
+++ src/rooms-states/rooms-state-controller.ts
@@ -1,15 +1,14 @@
 import { Controller } from '@nestjs/common';
 import { RoomsStatesService } from './rooms-state.service';
 import { CreateRoomStateDto } from './dto/create-room-state.dto';
-import { RoomStateEntity } from './entities/room-state.entity';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class RoomsStatesController {
   constructor(private readonly roomsStatesService: RoomsStatesService) {}
 
-  @MessagePattern({ cmd: 'save_room_state' })
-  async save(@Payload() request: CreateRoomStateDto): Promise<RoomStateEntity> {
+  @MessagePattern({ cmd: 'save.room.state' })
+  async save(@Payload() request: CreateRoomStateDto) {
     return this.roomsStatesService.save(request);
   }
 }
