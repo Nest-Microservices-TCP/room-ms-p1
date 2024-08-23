8f9207071e379f2c2029ecd8d96f04736d03d6dd
JosueEC <josueev.cruz@gmail.com>
2024-08-22 19:02:12 -0600

    [FEAT](rooms-state.controller): Expose the findOneById method

--- src/rooms-states/rooms-state-controller.ts
+++ src/rooms-states/rooms-state-controller.ts
@@ -2,13 +2,22 @@ import { Controller } from '@nestjs/common';
 import { RoomsStatesService } from './rooms-state.service';
 import { CreateRoomStateDto } from './dto/create-room-state.dto';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { RoomStateEntity } from './entities/room-state.entity';
+import { FindOneRoomStateByIdDto } from './dto/find-one-room-state-by-id.dto';
 
 @Controller()
 export class RoomsStatesController {
   constructor(private readonly roomsStatesService: RoomsStatesService) {}
 
   @MessagePattern({ cmd: 'save.room.state' })
-  async save(@Payload() request: CreateRoomStateDto) {
+  async save(@Payload() request: CreateRoomStateDto): Promise<RoomStateEntity> {
     return this.roomsStatesService.save(request);
+  }
+
+  @MessagePattern({ cmd: 'find.one.room.state' })
+  async findOneById(
+    @Payload() request: FindOneRoomStateByIdDto,
+  ): Promise<RoomStateEntity> {
+    return this.roomsStatesService.findOneById(request);
   }
 }
