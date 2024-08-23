ae9d57e94b03a7f7e684fd59af88078692eb8219
JosueEC <josueev.cruz@gmail.com>
2024-08-22 19:01:30 -0600

    [FEAT](database): Add the seed for the rooms-states table

---
+++ src/database/seeds/rooms-states.seed.sql
@@ -0,0 +1,17 @@
+truncate rooms_states ;
+
+INSERT INTO public.rooms_states
+(created_by, updated_by, status, deleted_at, "name")
+values
+	(NULL, NULL, 'active', NULL, 'Preparada'),
+	(NULL, NULL, 'active', NULL, 'Sucia'),
+	(NULL, NULL, 'active', NULL, 'A la venta'),
+	(NULL, NULL, 'active', NULL, 'Ocupada'),
+	(NULL, NULL, 'active', NULL, 'Supervisión'),
+	(NULL, NULL, 'active', NULL, 'Mantenimiento'),
+	(NULL, NULL, 'active', NULL, 'Limpieza'),
+	(NULL, NULL, 'active', NULL, 'Supervisión pendiente'),
+	(NULL, NULL, 'active', NULL, 'Supervisión en curso'),
+	(NULL, NULL, 'active', NULL, 'Reservada'),
+	(NULL, NULL, 'active', NULL, 'Bloqueada'),
+	(NULL, NULL, 'active', NULL, 'Room service');
