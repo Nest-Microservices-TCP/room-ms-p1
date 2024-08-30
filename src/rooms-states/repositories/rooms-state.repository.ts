c15f8a99fcc4216916e158d98f8f22439a55d915
JosueEC <josueev.cruz@gmail.com>
2024-08-29 19:28:33 -0600

    [REFACTOR](rooms-states.repository): use the basic repository and add the set-query-runner method

--- src/rooms-states/repositories/rooms-state.repository.ts
+++ src/rooms-states/repositories/rooms-state.repository.ts
@@ -2,40 +2,58 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { RoomStateEntity } from '../entities/room-state.entity';
 import { IRoomsStateRepository } from './interfaces/rooms-state.repository.interface';
 import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { QueryRunner, Repository } from 'typeorm';
 import { CreateRoomStateDto } from '../dto/create-room-state.dto';
+import { UpdateRoomStateDto } from '../dto';
 
 @Injectable()
 export class RoomsStatesRepository implements IRoomsStateRepository {
+  private roomsStatesRepository: Repository<RoomStateEntity>;
+
   constructor(
     @InjectRepository(RoomStateEntity)
-    private readonly roomStatesRepository: Repository<RoomStateEntity>,
-  ) {}
+    private readonly defaultRepository: Repository<RoomStateEntity>,
+  ) {
+    this.roomsStatesRepository = this.defaultRepository;
+  }
 
-  findById(id: string): Promise<RoomStateEntity> {
-    return this.roomStatesRepository.findOne({
-      where: { id },
-    });
+  setQueryRunner(queryRunner: QueryRunner): void {
+    if (queryRunner) {
+      this.roomsStatesRepository =
+        queryRunner.manager.getRepository(RoomStateEntity);
+    } else {
+      this.roomsStatesRepository = this.defaultRepository;
+    }
+  }
+
+  findOneById(id: string): Promise<RoomStateEntity> {
+    return this.roomsStatesRepository.findOne({ where: { id } });
   }
 
   findAll(): Promise<RoomStateEntity[]> {
-    return this.roomStatesRepository.find();
+    return this.roomsStatesRepository.find();
   }
 
-  create(roomState: Partial<RoomStateEntity>): RoomStateEntity {
-    return this.roomStatesRepository.create(roomState);
+  create(request: Partial<RoomStateEntity>): RoomStateEntity {
+    return this.roomsStatesRepository.create(request);
   }
 
   save(request: CreateRoomStateDto): Promise<RoomStateEntity> {
-    return this.roomStatesRepository.save(request);
+    //TODO: Validate repeated rooms
+    return this.roomsStatesRepository.save(request);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  update(request: UpdateRoomStateDto): Promise<RoomStateEntity> {
+    throw new Error('Method not implemented.');
   }
 
   async deleteById(id: string): Promise<RoomStateEntity> {
-    const roomState = await this.roomStatesRepository.findOne({
+    const roomState = await this.roomsStatesRepository.findOne({
       where: { id },
     });
 
-    await this.roomStatesRepository.delete(id);
+    await this.roomsStatesRepository.delete(id);
 
     return roomState;
   }
