9305a62fa06193ed875b20dc07312ac796d8e17a
JosueEC <josueev.cruz@gmail.com>
2024-08-31 21:23:29 -0600

    [FEAT](rooms.service): Add the save method and inject the rooms-repository

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { RoomsRepository } from './repositories/rooms.repository';
+import { RpcException } from '@nestjs/microservices';
+import { CreateRoomDto } from './dto';
+import { RoomEntity } from './entities/room.entity';
 
 @Injectable()
 export class RoomsService {
-  create() {
-    return 'Create a room';
+  constructor(private readonly roomsRepository: RoomsRepository) {}
+
+  save(request: CreateRoomDto): Promise<RoomEntity> {
+    try {
+      return this.roomsRepository.save(request);
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `Error to save room: ${error}`,
+      });
+    }
   }
 
   findOne() {
