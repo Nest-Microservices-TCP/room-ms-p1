281cbbd2942655f93cdbc6f95a7db5cab6121357
JosueEC <josueev.cruz@gmail.com>
2024-09-02 19:34:45 -0600

    [REFACTOR](rooms-state.controller): Rename the rooms-state controller file

--- src/rooms-states/rooms-state.module.ts
+++ src/rooms-states/rooms-state.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { RoomsStatesController } from './rooms-state-controller';
+import { RoomsStatesController } from './rooms-state.controller';
 import { RoomsStatesService } from './rooms-state.service';
 import { RoomStateEntity } from './entities/room-state.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
