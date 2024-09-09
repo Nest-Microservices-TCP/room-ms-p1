b702bfea950425a189d4beefb59f1a44ac5e7617
JosueEC <josueev.cruz@gmail.com>
2024-09-08 21:03:58 -0600

    [FEAT](rooms.module): Add the rooms-repository to the providers array

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
+import { RoomsRepository } from './repositories/rooms.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomEntity } from './entities/room.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RoomEntity])],
   controllers: [RoomsController],
-  providers: [RoomsService],
+  providers: [RoomsRepository, RoomsService],
 })
 export class RoomsModule {}
