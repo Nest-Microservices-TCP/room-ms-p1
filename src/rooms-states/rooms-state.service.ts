0bc221784121e325fbcc98f403a539308210b087
JosueEC <josueev.cruz@gmail.com>
2024-09-17 18:59:50 -0600

    [FIX](rooms-state.service): Use the correct path import for update and create dtos

--- src/rooms-states/rooms-state.service.ts
+++ src/rooms-states/rooms-state.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { RoomStateEntity } from './entities/room-state.entity';
-import { CreateRoomStateDto } from './dto/create-room-state.dto';
-import { FindOneRoomStateByIdDto } from './dto/find-one-room-state-by-id.dto';
 import { RoomsStatesRepository } from './repositories/rooms-state.repository';
-import { UpdateRoomStateDto } from './dto';
+import {
+  UpdateRoomStateDto,
+  CreateRoomStateDto,
+  FindOneRoomStateByIdDto,
+} from './dto/request';
 import { HandleRpcExceptions } from 'src/common/decorators';
 
 @Injectable()
