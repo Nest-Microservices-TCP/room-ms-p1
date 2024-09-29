0f6f95d244dbbd72505572879d12b9ccdf1967e4
JosueEC <josueev.cruz@gmail.com>
2024-09-28 21:07:02 -0600

    [FEAT](rents/dto/response): Add the rent-response dto class

---
+++ src/rents/dto/response/rent-response.dto.ts
@@ -0,0 +1,21 @@
+import { BaseResponseDto } from 'src/common/dto';
+
+export class RentResponseDto extends BaseResponseDto {
+  folio: number;
+  checkoutDate: Date;
+  departureAt: Date;
+  guests: number;
+  extraAccommodations: number;
+  extraPeople: number;
+  overtime: number;
+  entryType: 'a_pie' | 'auto'; //TODO: hacer el enum
+  totalIncome: number;
+  rentState: 'finalizada' | 'pagada' | 'pago_pendiente' | 'cancelada'; //TODO: hacer el enum, revisar una mejor forma
+  guestName: string;
+  accommodationType: 'hotel' | 'motel';
+  subtotals: string; //TODO: es un jsonb, revisar una mejor forma
+
+  roomId: string;
+  rateId: string;
+  hotelId: string;
+}
