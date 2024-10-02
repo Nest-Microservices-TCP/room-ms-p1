dec5efb04f82fd525bf7992e4a3e5f6b22d2db34
JosueEC <josueev.cruz@gmail.com>
2024-10-01 19:49:51 -0600

    [REFACTOR](room-state.entity): Add the room-state-id property as primary key

--- src/rooms/entities/room.entity.ts
+++ src/rooms/entities/room.entity.ts
@@ -1,8 +1,13 @@
 import { BaseEntity } from 'src/common';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'rooms' })
 export class RoomEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'room_id',
+  })
+  room_id: string;
+
   @Column({
     name: 'number',
     type: 'int',
