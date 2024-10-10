26ccb09b9a9306b1bc5f17d8441e6085a1e7dcf3
JosueEC <josueev.cruz@gmail.com>
2024-10-09 20:14:30 -0600

    [FEAT](app.module): Inject the rates-module in the imports array

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RoomsModule } from './rooms/rooms.module';
 import { PostgresDBModule } from './database/postgresdb.module';
 import { RoomsStatesModule } from './rooms-states/rooms-state.module';
+import { RatesModule } from './rates/rates.module';
 
 @Module({
-  imports: [PostgresDBModule, RoomsModule, RoomsStatesModule],
+  imports: [PostgresDBModule, RoomsModule, RoomsStatesModule, RatesModule],
   controllers: [],
   providers: [],
 })
