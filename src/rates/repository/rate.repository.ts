1f61b7f473e862977fd31495b8387082185609dd
JosueEC <josueev.cruz@gmail.com>
2024-10-12 19:42:28 -0600

    [FEAT](rate.repository): Inject the default repository and define the set-query-runner method

--- src/rates/repository/rate.repository.ts
+++ src/rates/repository/rate.repository.ts
@@ -1,13 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { QueryRunner } from 'typeorm';
+import { QueryRunner, Repository } from 'typeorm';
 import { CreateRateDto, UpdateRateDto } from '../dto/request';
 import { RateEntity } from '../entity/rate.entity';
 import { IRatesRepository } from './interfaces/rate.repository.interface';
+import { InjectRepository } from '@nestjs/typeorm';
 
 export class RatesRepository implements IRatesRepository {
-  setQueryRunner(queryRunner: QueryRunner): void {
-    throw new Error('Method not implemented.');
+  private ratesRepository: Repository<RateEntity>;
+
+  constructor(
+    @InjectRepository(RateEntity)
+    private readonly defaultRepository: Repository<RateEntity>,
+  ) {
+    this.ratesRepository = defaultRepository;
   }
+
+  setQueryRunner(queryRunner: QueryRunner): void {
+    if (queryRunner) {
+      this.ratesRepository = queryRunner.manager.getRepository(RateEntity);
+    } else {
+      this.ratesRepository = this.defaultRepository;
+    }
+  }
+
   findAll(): Promise<RateEntity[]> {
     throw new Error('Method not implemented.');
   }
