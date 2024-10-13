1ffec439383478319fd08de82970a82925435955
JosueEC <josueev.cruz@gmail.com>
2024-10-12 19:52:10 -0600

    [FEAT](rates.module): Inject the typeorm-module, rates-controller, rates-repository and rates-service to the arrays module

--- src/rates/rates.module.ts
+++ src/rates/rates.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { RatesService } from './rates.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RateEntity } from './entity/rate.entity';
+import { RatesController } from './rates.controller';
+import { RatesRepository } from './repository/rate.repository';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([RateEntity])],
+  controllers: [RatesController],
+  providers: [RatesRepository, RatesService],
+})
 export class RatesModule {}
