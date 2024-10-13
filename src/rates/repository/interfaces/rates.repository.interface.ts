a97397b0da91246a98f2d272c59034f2003130e3
JosueEC <josueev.cruz@gmail.com>
2024-10-12 20:02:38 -0600

    [REFACTOR](rates/repository): Rename the rate-repository-interface from singular to plural

--- src/rates/repository/interfaces/rates.repository.interface.ts
+++ src/rates/repository/interfaces/rates.repository.interface.ts
@@ -1,6 +1,6 @@
 import { IBaseRepository } from 'src/common/interfaces';
-import { CreateRateDto, UpdateRateDto } from 'src/rates/dto/request';
 import { RateEntity } from 'src/rates/entity/rate.entity';
+import { CreateRateDto, UpdateRateDto } from 'src/rates/dto/request';
 
 export interface IRatesRepository
   extends IBaseRepository<RateEntity, CreateRateDto, UpdateRateDto> {}
