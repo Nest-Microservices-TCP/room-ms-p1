54506e0ff0f7172aeeab7a4d3c01e0ff66166b4a
JosueEC <josueev.cruz@gmail.com>
2024-10-12 19:51:28 -0600

    [FEAT](rates.controller): Expose the find-all method

--- src/rates/rates.controller.ts
+++ src/rates/rates.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { RatesService } from './rates.service';
+import { RateResponseDto } from './dto/response';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
-export class RatesController {}
+export class RatesController {
+  constructor(private readonly ratesService: RatesService) {}
+
+  @MessagePattern({ cmd: 'find.all.rates' })
+  findAll(): Promise<RateResponseDto[]> {
+    return this.ratesService.findAll();
+  }
+}
