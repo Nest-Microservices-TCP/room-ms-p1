6f8400323e2439456e583197ac1d8325819b7b59
JosueEC <josueev.cruz@gmail.com>
2024-10-12 19:59:00 -0600

    [FEAT](rates.controller): Expose the find-one-by-id endpoint

--- src/rates/rates.controller.ts
+++ src/rates/rates.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { RatesService } from './rates.service';
 import { RateResponseDto } from './dto/response';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class RatesController {
@@ -10,5 +10,10 @@ export class RatesController {
   @MessagePattern({ cmd: 'find.all.rates' })
   findAll(): Promise<RateResponseDto[]> {
     return this.ratesService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'find.one.rate.by.id' })
+  findOneById(@Payload('rateId') rateId: string): Promise<RateResponseDto> {
+    return this.ratesService.findOneById(rateId);
   }
 }
