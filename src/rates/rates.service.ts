0db590e10def49e59f3047fe0e0c6aa3d9a78aea
JosueEC <josueev.cruz@gmail.com>
2024-10-12 19:51:07 -0600

    [FEAT](rates.service): Add the find-all method

--- src/rates/rates.service.ts
+++ src/rates/rates.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { RateResponseDto } from './dto/response';
+import { plainToInstance } from 'class-transformer';
+import { HandleRpcExceptions } from 'src/common/decorators';
 import { RatesRepository } from './repository/rate.repository';
 
 @Injectable()
 export class RatesService {
   constructor(private readonly ratesRepository: RatesRepository) {}
+
+  @HandleRpcExceptions()
+  async findAll(): Promise<RateResponseDto[]> {
+    const rates = await this.ratesRepository.findAll();
+
+    return plainToInstance(RateResponseDto, rates, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
