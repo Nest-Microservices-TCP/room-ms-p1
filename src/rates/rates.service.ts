a101ddd43f1c1644248b748a921ea3f2679233a3
JosueEC <josueev.cruz@gmail.com>
2024-10-13 19:47:25 -0600

    [FEAT](rates.service): Add the save, update and delete-by-id methods

--- src/rates/rates.service.ts
+++ src/rates/rates.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { RateResponseDto } from './dto/response';
 import { plainToInstance } from 'class-transformer';
 import { HandleRpcExceptions } from 'src/common/decorators';
-import { RatesRepository } from './repository/rate.repository';
+import { CreateRateDto, UpdateRateDto } from './dto/request';
+import { RatesRepository } from './repository/rates.repository';
 
 @Injectable()
 export class RatesService {
@@ -20,6 +21,33 @@ export class RatesService {
   @HandleRpcExceptions()
   async findOneById(rateId: string): Promise<RateResponseDto> {
     const rate = await this.ratesRepository.findOneById(rateId);
+
+    return plainToInstance(RateResponseDto, rate, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  @HandleRpcExceptions()
+  async save(request: CreateRateDto): Promise<RateResponseDto> {
+    const rate = await this.ratesRepository.save(request);
+
+    return plainToInstance(RateResponseDto, rate, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  @HandleRpcExceptions()
+  async update(request: UpdateRateDto): Promise<RateResponseDto> {
+    const rate = await this.ratesRepository.update(request);
+
+    return plainToInstance(RateResponseDto, rate, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  @HandleRpcExceptions()
+  async deleteById(rateId: string): Promise<RateResponseDto> {
+    const rate = await this.ratesRepository.deleteById(rateId);
 
     return plainToInstance(RateResponseDto, rate, {
       excludeExtraneousValues: true,
