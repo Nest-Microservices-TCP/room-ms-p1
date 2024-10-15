5ec9c104deb4dc6cbc27ee4c94c19fcde6b614cc
JosueEC <josueev.cruz@gmail.com>
2024-10-14 19:53:24 -0600

    [FEAT](rents.repository): Implement the rents-repository-interface

--- src/rents/repository/rents.repository.ts
+++ src/rents/repository/rents.repository.ts
@@ -1,1 +1,40 @@
-export class RentsRepository {}
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { QueryRunner, Repository } from 'typeorm';
+import { CreateRentDto, UpdateRentDto } from '../dto/request';
+import { RentEntity } from '../entity';
+import { IRentsRepository } from './interfaces/rents.repository.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+
+export class RentsRepository implements IRentsRepository {
+  private rentsRepository: Repository<RentEntity>;
+
+  constructor(
+    @InjectRepository(RentEntity)
+    private readonly defaultRepository: Repository<RentEntity>,
+  ) {
+    this.rentsRepository = this.defaultRepository;
+  }
+
+  setQueryRunner(queryRunner: QueryRunner): void {
+    throw new Error('Method not implemented.');
+  }
+
+  findAll(): Promise<RentEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  findOneById(id: string): Promise<RentEntity> {
+    throw new Error('Method not implemented.');
+  }
+  create(request: Partial<RentEntity>): RentEntity {
+    throw new Error('Method not implemented.');
+  }
+  save(request: CreateRentDto): Promise<RentEntity> {
+    throw new Error('Method not implemented.');
+  }
+  update(request: UpdateRentDto): Promise<RentEntity> {
+    throw new Error('Method not implemented.');
+  }
+  deleteById(id: string): Promise<RentEntity> {
+    throw new Error('Method not implemented.');
+  }
+}
