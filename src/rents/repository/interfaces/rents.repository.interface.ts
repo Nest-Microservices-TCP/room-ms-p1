0ff1c603aabd7f5f8693e15c3d3f1962757220b0
JosueEC <josueev.cruz@gmail.com>
2024-10-14 19:52:44 -0600

    [FEAT](rents.repository.interface): Extends from the base-repository and pass it the entity, and create and update rent dtos

--- src/rents/repository/interfaces/rents.repository.interface.ts
+++ src/rents/repository/interfaces/rents.repository.interface.ts
@@ -1,1 +1,6 @@
-export class IRentsRepository {}
+import { IBaseRepository } from 'src/common/interfaces';
+import { CreateRentDto, UpdateRentDto } from 'src/rents/dto/request';
+import { RentEntity } from 'src/rents/entity';
+
+export interface IRentsRepository
+  extends IBaseRepository<RentEntity, CreateRentDto, UpdateRentDto> {}
