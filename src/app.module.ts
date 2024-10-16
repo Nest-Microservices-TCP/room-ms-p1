c03ec6748969e12d417cb05996901e5e2416e509
JosueEC <josueev.cruz@gmail.com>
2024-10-15 19:35:18 -0600

    [FEAT](app.module): Inject the rents-module to the imports array

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { RoomsModule } from './rooms/rooms.module';
 import { PostgresDBModule } from './database/postgresdb.module';
 import { RoomsStatesModule } from './rooms-states/rooms-state.module';
 import { RatesModule } from './rates/rates.module';
+import { RentsModule } from './rents/rents.module';
 
 @Module({
-  imports: [PostgresDBModule, RoomsModule, RoomsStatesModule, RatesModule],
+  imports: [
+    RoomsModule,
+    RentsModule,
+    RatesModule,
+    PostgresDBModule,
+    RoomsStatesModule,
+  ],
   controllers: [],
   providers: [],
 })
