15f2f3a52eecef8971c098b0be71391d95df3d68
JosueEC <josueev.cruz@gmail.com>
2024-10-15 19:31:59 -0600

    [FEAT](rents.module): Inject the typeorm-module, rents-controller, rents-repository and rents-service to the dependencies arrays

--- src/rents/rents.module.ts
+++ src/rents/rents.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { RentEntity } from './entity';
+import { RentsService } from './rents.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RentsController } from './rents.controller';
+import { RentsRepository } from './repository/rents.repository';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([RentEntity])],
+  controllers: [RentsController],
+  providers: [RentsRepository, RentsService],
+})
 export class RentsModule {}
