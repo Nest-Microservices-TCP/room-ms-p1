2a88918d0e6d16780ddc0f9e3bf116256d05838e
JosueEC <josueev.cruz@gmail.com>
2024-10-16 19:46:20 -0600

    [FEAT](rents.service): Add the find-all and find-one-by-id methods

--- src/rents/rents.service.ts
+++ src/rents/rents.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { RentsRepository } from './repository/rents.repository';
+import { RentEntity } from './entity';
+import { HandleRpcExceptions } from 'src/common/decorators';
 
 @Injectable()
 export class RentsService {
   constructor(private readonly rentsRepository: RentsRepository) {}
+
+  @HandleRpcExceptions()
+  async findAll(): Promise<RentEntity[]> {
+    return this.rentsRepository.findAll();
+  }
+
+  @HandleRpcExceptions()
+  async findOneById(rentId: string): Promise<RentEntity> {
+    return this.rentsRepository.findOneById(rentId);
+  }
 }
