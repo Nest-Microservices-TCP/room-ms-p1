89498bb2af843eac886455c95faf0b3073f822f7
JosueEC <josueev.cruz@gmail.com>
2024-10-16 19:39:10 -0600

    [FEAT](update-rent.dto): Add dtos properties validations

--- src/rents/dto/request/update-rent.dto.ts
+++ src/rents/dto/request/update-rent.dto.ts
@@ -1,21 +1,88 @@
-import { AccommodationType, EntryType } from 'src/rents/enum';
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUUID,
+  MaxLength,
+  Min,
+} from 'class-validator';
+import { AccommodationType, EntryType, RentState } from 'src/rents/enum';
 
 export class UpdateRentDto {
+  @IsUUID()
   rentId: string;
-  checkoutDate: Date;
-  departureAt: Date;
+
+  @IsDate()
+  @IsOptional()
+  checkoutDate?: Date;
+
+  @IsDate()
+  @IsOptional()
+  departureAt?: Date;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  @Min(1)
   guests: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  @Min(1)
   extraAccommodations: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  @Min(1)
   extraPeople: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  @Min(1)
   overtime: number;
+
+  @IsEnum(EntryType)
+  @IsOptional()
   entryType: EntryType;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  @Min(1)
   totalIncome: number;
-  rentState: 'finalizada' | 'pagada' | 'pago_pendiente' | 'cancelada'; //TODO: hacer el enum, revisar una mejor forma
+
+  @IsEnum(RentState)
+  @IsOptional()
+  rentState: RentState;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @IsOptional()
   guestName: string;
+
+  @IsEnum(AccommodationType)
+  @IsOptional()
   accommodationType: AccommodationType;
+
   subtotals: string; //TODO: es un jsonb, revisar una mejor forma
 
+  @IsUUID()
+  @IsOptional()
   roomId: string;
+
+  @IsUUID()
+  @IsOptional()
   rateId: string;
+
+  @IsUUID()
+  @IsOptional()
   hotelId: string;
 }
