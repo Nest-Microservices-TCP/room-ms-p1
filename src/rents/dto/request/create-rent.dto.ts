1b4f63e68bf9b3e48a28dbcc031cedba5cb696ab
JosueEC <josueev.cruz@gmail.com>
2024-10-16 19:39:01 -0600

    [FEAT](create-rent.dto): Add dtos properties validations

--- src/rents/dto/request/create-rent.dto.ts
+++ src/rents/dto/request/create-rent.dto.ts
@@ -1,11 +1,46 @@
+import {
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
 import { AccommodationType, EntryType } from 'src/rents/enum';
 
 export class CreateRentDto {
+  @IsString()
+  @MaxLength(255)
+  @IsOptional()
   guestName: string;
+
+  @IsEnum(AccommodationType)
   accommodationType: AccommodationType;
+
+  @IsEnum(EntryType)
   entryType: EntryType;
-  guests: number;
+
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  guestsNumber: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  @Min(1)
   extraAccommodations: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  @Min(1)
   extraPeople: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  @Min(1)
   overtime: number;
 }
