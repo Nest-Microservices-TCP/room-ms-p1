999d03ef41b0c7bf0ed85c78f456d89568b116b2
JosueEC <josueev.cruz@gmail.com>
2024-10-17 20:42:56 -0600

    [FEAT](rents.controller): Expose the find-all and find-one-by-id endpoints

--- src/rents/rents.controller.ts
+++ src/rents/rents.controller.ts
@@ -1,7 +1,28 @@
 import { Controller } from '@nestjs/common';
 import { RentsService } from './rents.service';
+import { RentResponseDto } from './dto/response';
+import { plainToInstance } from 'class-transformer';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class RentsController {
   constructor(private readonly rentsService: RentsService) {}
+
+  @MessagePattern({ cmd: 'find.all.rents' })
+  async findAll(): Promise<RentResponseDto[]> {
+    const rents = await this.rentsService.findAll();
+
+    return plainToInstance(RentResponseDto, rents, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  @MessagePattern({ cmd: 'find.one.rent.by.id' })
+  async findOneById(rentId): Promise<RentResponseDto> {
+    const rent = await this.rentsService.findOneById(rentId);
+
+    return plainToInstance(RentResponseDto, rent, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
