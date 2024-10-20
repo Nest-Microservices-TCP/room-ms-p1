e3d6ec2ca6d7979fb609b95acac92d2bd1fd3532
JosueEC <josueev.cruz@gmail.com>
2024-10-19 20:39:55 -0600

    [FEAT](extras.repository.interface): Extends from the base-repository interface

--- src/extras/repository/interfaces/exras.repository.interface.ts
+++ src/extras/repository/interfaces/exras.repository.interface.ts
@@ -1,1 +1,6 @@
-export interface IExtrasRepository {}
+import { IBaseRepository } from 'src/common/interfaces';
+import { ExtraEntity } from 'src/extras/entity/extra.entity';
+import { CreateExtraDto, UpdateExtraDto } from 'src/extras/dto/request';
+
+export interface IExtrasRepository
+  extends IBaseRepository<ExtraEntity, CreateExtraDto, UpdateExtraDto> {}
