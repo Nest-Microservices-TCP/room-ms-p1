b9aea4b29d91f1412ff9ef7b5edbb701aff194fa
JosueEC <josueev.cruz@gmail.com>
2024-10-20 17:20:11 -0600

    [FEAT](extras.service): Add the find-all and find-one-by-id methods

--- src/extras/extras.service.ts
+++ src/extras/extras.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ExtrasRepository } from './repository/extras.repository';
+import { ExtraResponseDto } from './dto/response';
+import { plainToInstance } from 'class-transformer';
+import { HandleRpcExceptions } from 'src/common/decorators';
 
 @Injectable()
 export class ExtrasService {
   constructor(private readonly extrasRepository: ExtrasRepository) {}
+
+  @HandleRpcExceptions()
+  async findAll(): Promise<ExtraResponseDto[]> {
+    const extras = await this.extrasRepository.findAll();
+
+    return plainToInstance(ExtraResponseDto, extras, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  @HandleRpcExceptions()
+  async findOneById(extraId: string): Promise<ExtraResponseDto> {
+    const extra = await this.extrasRepository.findOneById(extraId);
+
+    return plainToInstance(ExtraResponseDto, extra, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
