18d32a01e2f8272da9a7c935bca8c6a05a576455
JosueEC <josueev.cruz@gmail.com>
2024-10-20 17:29:30 -0600

    [FEAT](extras.controller): Expose the save and update endpoints

--- src/extras/extras.controller.ts
+++ src/extras/extras.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { ExtrasService } from './extras.service';
 import { ExtraResponseDto } from './dto/response';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateExtraDto, UpdateExtraDto } from './dto/request';
 
 @Controller()
 export class ExtrasController {
@@ -15,5 +16,15 @@ export class ExtrasController {
   @MessagePattern({ cmd: 'find.one.extra.by.id' })
   findOneById(@Payload('extraId') extraId: string): Promise<ExtraResponseDto> {
     return this.extrasService.findOneById(extraId);
+  }
+
+  @MessagePattern({ cmd: 'save.extra' })
+  save(@Payload() request: CreateExtraDto): Promise<ExtraResponseDto> {
+    return this.extrasService.save(request);
+  }
+
+  @MessagePattern({ cmd: 'update.extra' })
+  update(@Payload() request: UpdateExtraDto): Promise<ExtraResponseDto> {
+    return this.extrasService.update(request);
   }
 }
