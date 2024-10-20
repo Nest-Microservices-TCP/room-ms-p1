1314727f9b020a00d62ec58f031f51882ed17dcb
JosueEC <josueev.cruz@gmail.com>
2024-10-20 17:27:18 -0600

    [FEAT](extras.controller): Expose the find-all and find-one-by-id endpoints

--- src/extras/extras.controller.ts
+++ src/extras/extras.controller.ts
@@ -1,7 +1,19 @@
 import { Controller } from '@nestjs/common';
 import { ExtrasService } from './extras.service';
+import { ExtraResponseDto } from './dto/response';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class ExtrasController {
   constructor(private readonly extrasService: ExtrasService) {}
+
+  @MessagePattern({ cmd: 'find.all.extras' })
+  findAll(): Promise<ExtraResponseDto[]> {
+    return this.extrasService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'find.one.extra.by.id' })
+  findOneById(@Payload('extraId') extraId: string): Promise<ExtraResponseDto> {
+    return this.extrasService.findOneById(extraId);
+  }
 }
