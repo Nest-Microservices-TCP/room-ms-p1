92a0ca5fd890cebabfcefce93b2dcf109d8505b0
JosueEC <josueev.cruz@gmail.com>
2024-10-19 20:38:27 -0600

    [FEAT](extras/dto/request): Define and validate the update-extra dto properties

--- src/extras/dto/request/update-extra.dto.ts
+++ src/extras/dto/request/update-extra.dto.ts
@@ -1,1 +1,18 @@
-export class UpdateExtraDto {}
+import {
+  IsUUID,
+  IsString,
+  MaxLength,
+  IsOptional,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class UpdateExtraDto {
+  @IsUUID()
+  extraId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @IsOptional()
+  name?: string;
+}
