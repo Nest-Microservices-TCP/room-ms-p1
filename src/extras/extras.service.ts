35384211737e13e7fd4652950336910b8f823559
JosueEC <josueev.cruz@gmail.com>
2024-10-20 17:23:00 -0600

    [FEAT](extras.service): Add the save and update methods

--- src/extras/extras.service.ts
+++ src/extras/extras.service.ts
@@ -3,6 +3,7 @@ import { ExtrasRepository } from './repository/extras.repository';
 import { ExtraResponseDto } from './dto/response';
 import { plainToInstance } from 'class-transformer';
 import { HandleRpcExceptions } from 'src/common/decorators';
+import { CreateExtraDto, UpdateExtraDto } from './dto/request';
 
 @Injectable()
 export class ExtrasService {
@@ -20,6 +21,24 @@ export class ExtrasService {
   @HandleRpcExceptions()
   async findOneById(extraId: string): Promise<ExtraResponseDto> {
     const extra = await this.extrasRepository.findOneById(extraId);
+
+    return plainToInstance(ExtraResponseDto, extra, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  @HandleRpcExceptions()
+  async save(request: CreateExtraDto): Promise<ExtraResponseDto> {
+    const extra = await this.extrasRepository.save(request);
+
+    return plainToInstance(ExtraResponseDto, extra, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  @HandleRpcExceptions()
+  async update(request: UpdateExtraDto): Promise<ExtraResponseDto> {
+    const extra = await this.extrasRepository.update(request);
 
     return plainToInstance(ExtraResponseDto, extra, {
       excludeExtraneousValues: true,
