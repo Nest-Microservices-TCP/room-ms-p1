4e3e7e5625fbee8f4cf536762b94865604123ad6
JosueEC <josueev.cruz@gmail.com>
2024-10-19 20:40:24 -0600

    [FEAT](extras.repository): Implements the extras-repository interface

--- src/extras/repository/extras.repository.ts
+++ src/extras/repository/extras.repository.ts
@@ -1,1 +1,29 @@
-export class ExtrasRepository {}
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { QueryRunner } from 'typeorm';
+import { ExtraEntity } from '../entity/extra.entity';
+import { CreateExtraDto, UpdateExtraDto } from '../dto/request';
+import { IExtrasRepository } from './interfaces/exras.repository.interface';
+
+export class ExtrasRepository implements IExtrasRepository {
+  setQueryRunner(queryRunner: QueryRunner): void {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): Promise<ExtraEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  findOneById(id: string): Promise<ExtraEntity> {
+    throw new Error('Method not implemented.');
+  }
+  create(request: Partial<ExtraEntity>): ExtraEntity {
+    throw new Error('Method not implemented.');
+  }
+  save(request: CreateExtraDto): Promise<ExtraEntity> {
+    throw new Error('Method not implemented.');
+  }
+  update(request: UpdateExtraDto): Promise<ExtraEntity> {
+    throw new Error('Method not implemented.');
+  }
+  deleteById(id: string): Promise<ExtraEntity> {
+    throw new Error('Method not implemented.');
+  }
+}
