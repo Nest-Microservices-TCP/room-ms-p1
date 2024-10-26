8f8e89bbbeffe20f7d857735c34456d29b0a4da9
JosueEC <josueev.cruz@gmail.com>
2024-10-25 19:53:55 -0600

    [FEAT](rooms.repository): Implement the extra common repository methods

--- src/rooms/repositories/rooms.repository.ts
+++ src/rooms/repositories/rooms.repository.ts
@@ -1,12 +1,18 @@
-import { Status } from 'src/common/enums';
-import { RoomEntity } from '../entity/room.entity';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { EntityNotFoundException } from 'src/common/exceptions/custom/entity-not-found.exception';
+import { IRoomsRepository } from './interfaces/rooms.repository.interface';
+import { FailedRemoveException } from 'src/common/exceptions/custom';
+import { CreateRoomDto, UpdateRoomDto } from '../dto/request';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ConflictException } from '@nestjs/common';
-import { CreateRoomDto, UpdateRoomDto } from '../dto/request';
-import { QueryRunner, Repository, UpdateResult } from 'typeorm';
-import { FailedDeleteException } from 'src/common/exceptions/custom';
-import { IRoomsRepository } from './interfaces/rooms.repository.interface';
-import { EntityNotFoundException } from 'src/common/exceptions/custom/entity-not-found.exception';
+import { RoomEntity } from '../entity/room.entity';
+import { Status } from 'src/common/enums';
+import {
+  Repository,
+  QueryRunner,
+  UpdateResult,
+  FindOptionsWhere,
+} from 'typeorm';
 
 export class RoomsRepository implements IRoomsRepository {
   private roomsRepository: Repository<RoomEntity>;
@@ -81,9 +87,43 @@ export class RoomsRepository implements IRoomsRepository {
     });
 
     if (result.affected === 0) {
-      throw new FailedDeleteException('room');
+      throw new FailedRemoveException('room');
     }
 
     return this.findOneById(roomId);
+  }
+
+  findByIds(ids: string[]): Promise<RoomEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  findByCriteria(criteria: FindOptionsWhere<RoomEntity>): Promise<RoomEntity> {
+    throw new Error('Method not implemented.');
+  }
+  findWithRelations(relations: string[]): Promise<RoomEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  count(criteria: FindOptionsWhere<RoomEntity>): Promise<number> {
+    throw new Error('Method not implemented.');
+  }
+  paginate(page: number, limit: number): Promise<[RoomEntity[], number]> {
+    throw new Error('Method not implemented.');
+  }
+  softDelete(id: string): Promise<RoomEntity> {
+    throw new Error('Method not implemented.');
+  }
+  restore(id: string): Promise<RoomEntity> {
+    throw new Error('Method not implemented.');
+  }
+  exists(criteria: FindOptionsWhere<RoomEntity>): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+  bulkSave(entities: RoomEntity[]): Promise<RoomEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  bulkUpdate(entities: RoomEntity[]): Promise<RoomEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  customQuery(query: string, params: any[]): Promise<any> {
+    throw new Error('Method not implemented.');
   }
 }
