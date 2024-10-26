8e4318dc30f2652d6447d08808fda91751b0b554
JosueEC <josueev.cruz@gmail.com>
2024-10-25 19:51:44 -0600

    [FEAT](base.repository.interface): Add extra common repository methods

--- src/common/interfaces/base-repository.interface.ts
+++ src/common/interfaces/base-repository.interface.ts
@@ -1,4 +1,4 @@
-import { QueryRunner } from 'typeorm';
+import { FindOptionsWhere, QueryRunner } from 'typeorm';
 
 export interface IBaseRepository<T, CreateDto, UpdateDto> {
   setQueryRunner(queryRunner: QueryRunner): void;
@@ -8,4 +8,16 @@ export interface IBaseRepository<T, CreateDto, UpdateDto> {
   save(request: CreateDto): Promise<T>;
   update(request: UpdateDto): Promise<T>;
   deleteById(id: string): Promise<T>;
+
+  findByIds(ids: string[]): Promise<T[]>;
+  findByCriteria(criteria: FindOptionsWhere<T>): Promise<T>;
+  findWithRelations(relations: string[]): Promise<T[]>;
+  count(criteria: FindOptionsWhere<T>): Promise<number>;
+  paginate(page: number, limit: number): Promise<[T[], number]>;
+  softDelete(id: string): Promise<T>;
+  restore(id: string): Promise<T>;
+  exists(criteria: FindOptionsWhere<T>): Promise<boolean>;
+  bulkSave(entities: T[]): Promise<T[]>;
+  bulkUpdate(entities: T[]): Promise<T[]>;
+  customQuery(query: string, params: any[]): Promise<any>;
 }
