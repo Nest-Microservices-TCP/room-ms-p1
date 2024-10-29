e1c4bca44b1f897503ddc14160929a0eb14d9617
JosueEC <josueev.cruz@gmail.com>
2024-10-28 20:38:41 -0600

    [FIX](rooms-state.controller): Use the correct import path for room-state-response-dto

--- src/rooms-states/rooms-state.controller.ts
+++ src/rooms-states/rooms-state.controller.ts
@@ -1,6 +1,6 @@
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { RoomsStatesService } from './rooms-state.service';
-import { RoomStateResponse } from './dto/response';
+import { RoomStateResponseDto } from './dto/response';
 import { Controller } from '@nestjs/common';
 import {
   CreateRoomStateDto,
@@ -15,38 +15,38 @@ export class RoomsStatesController {
   @MessagePattern({ cmd: 'save.roomState' })
   async save(
     @Payload() request: CreateRoomStateDto,
-  ): Promise<RoomStateResponse> {
+  ): Promise<RoomStateResponseDto> {
     return this.roomsStatesService.save(request);
   }
 
   @MessagePattern({ cmd: 'find.one.roomState' })
   async findOneById(
     @Payload() request: FindOneRoomStateByIdDto,
-  ): Promise<RoomStateResponse> {
+  ): Promise<RoomStateResponseDto> {
     return this.roomsStatesService.findOneById(request);
   }
 
   @MessagePattern({ cmd: 'find.rooms.states.by.ids' })
-  async findByIds(roomsStatesIds: string[]): Promise<RoomStateResponse[]> {
+  async findByIds(roomsStatesIds: string[]): Promise<RoomStateResponseDto[]> {
     return this.roomsStatesService.findByIds(roomsStatesIds);
   }
 
   @MessagePattern({ cmd: 'find.all.roomsStates' })
-  async findAll(): Promise<RoomStateResponse[]> {
+  async findAll(): Promise<RoomStateResponseDto[]> {
     return this.roomsStatesService.findAll();
   }
 
   @MessagePattern({ cmd: 'update.roomState' })
   async update(
     @Payload() request: UpdateRoomStateDto,
-  ): Promise<RoomStateResponse> {
+  ): Promise<RoomStateResponseDto> {
     return this.roomsStatesService.update(request);
   }
 
   @MessagePattern({ cmd: 'remove.roomState.by.id' })
   async remove(
     @Payload('roomStateId') roomStateId: string,
-  ): Promise<RoomStateResponse> {
+  ): Promise<RoomStateResponseDto> {
     return this.roomsStatesService.remove(roomStateId);
   }
 }
