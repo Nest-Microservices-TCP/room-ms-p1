e9c17be37ec8028887be2785452a994a4785dea9
JosueEC <josueev.cruz@gmail.com>
2024-10-29 20:11:23 -0600

    [FIX](app.module): Use the correct import path for rooms-states-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { RoomsModule } from './rooms/rooms.module';
+import { RoomsStatesModule } from './rooms-states/rooms-states.module';
 import { PostgresDBModule } from './database/postgresdb.module';
-import { RoomsStatesModule } from './rooms-states/rooms-state.module';
+import { RoomsModule } from './rooms/rooms.module';
 import { RatesModule } from './rates/rates.module';
 import { RentsModule } from './rents/rents.module';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
