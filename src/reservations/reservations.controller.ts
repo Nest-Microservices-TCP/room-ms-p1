235fdda1a4a55f26b59b942b83268eec442bae1d
JosueEC <josueev.cruz@gmail.com>
2024-11-16 20:40:25 -0600

    [FEAT](reservations.controller): Inject the reservations-service

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
 
 @Controller()
-export class ReservationsController {}
+export class ReservationsController {
+  constructor(private readonly reservationsService: ReservationsService) {}
+}
