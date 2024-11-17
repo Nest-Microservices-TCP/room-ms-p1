078b6d353a9d691990b9ec1d6fccdb1c231cbf49
JosueEC <josueev.cruz@gmail.com>
2024-11-16 20:36:45 -0600

    [FEAT](update-reservation.dto): Define the dto properties

--- src/reservations/dto/request/update-reservation.dto.ts
+++ src/reservations/dto/request/update-reservation.dto.ts
@@ -1,1 +1,11 @@
-export class UpdateReservationDto {}
+import { ReservationOrigin, ReservationState } from 'src/reservations/enum';
+
+export class UpdateReservationDto {
+  checkoutDate: Date;
+  departureDate: Date;
+  peopleQuantity: number;
+  state: ReservationState;
+  total: number;
+  origin: ReservationOrigin;
+  balance: number;
+}
