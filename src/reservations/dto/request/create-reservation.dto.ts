521007a12700cbd17284d42bacc7dcf4c5387cf3
JosueEC <josueev.cruz@gmail.com>
2024-11-16 20:36:33 -0600

    [FEAT](create-reservation.dto): Define the dto properties

--- src/reservations/dto/request/create-reservation.dto.ts
+++ src/reservations/dto/request/create-reservation.dto.ts
@@ -1,1 +1,18 @@
-export class CreateReservationDto {}
+import { IsDate, IsEnum, IsNumber, IsPositive, Min } from 'class-validator';
+import { ReservationOrigin } from 'src/reservations/enum';
+
+export class CreateReservationDto {
+  @IsDate()
+  checkoutDate: Date;
+
+  @IsDate()
+  departureDate: Date;
+
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  peopleQuantity: number;
+
+  @IsEnum(ReservationOrigin)
+  origin: ReservationOrigin;
+}
