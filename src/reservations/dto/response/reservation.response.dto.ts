714f9ca5d508a7358459b506cf8747f2dec08490
JosueEC <josueev.cruz@gmail.com>
2024-11-17 11:55:08 -0600

    [FEAT](reservations/dto/response): Expose the reservations-response-dto properties

--- src/reservations/dto/response/reservation.response.dto.ts
+++ src/reservations/dto/response/reservation.response.dto.ts
@@ -1,12 +1,28 @@
 import { ReservationOrigin, ReservationState } from 'src/reservations/enum';
+import { Expose } from 'class-transformer';
 
 export class ReservationResponseDto {
+  @Expose()
   reservation_id: string;
+
+  @Expose()
   checkoutDate: Date;
+
+  @Expose()
   departureDate: Date;
+
+  @Expose()
   peopleQuantity: number;
+
+  @Expose()
   state: ReservationState;
+
+  @Expose()
   total: number;
+
+  @Expose()
   origin: ReservationOrigin;
+
+  @Expose()
   balance: number;
 }
