2bb6e8df9da23349b2b2224a9392c959b1ffb075
JosueEC <josueev.cruz@gmail.com>
2024-11-17 11:54:38 -0600

    [FEAT](reservations/dto/request): Add the validations for update-reservations-dto properties

--- src/reservations/dto/request/update-reservation.dto.ts
+++ src/reservations/dto/request/update-reservation.dto.ts
@@ -1,11 +1,50 @@
 import { ReservationOrigin, ReservationState } from 'src/reservations/enum';
+import {
+  Min,
+  IsDate,
+  IsEnum,
+  IsUUID,
+  IsNumber,
+  IsPositive,
+  IsOptional,
+} from 'class-validator';
 
 export class UpdateReservationDto {
-  checkoutDate: Date;
-  departureDate: Date;
-  peopleQuantity: number;
-  state: ReservationState;
-  total: number;
-  origin: ReservationOrigin;
+  @IsUUID('4')
+  reservation_id: string;
+
+  @IsDate()
+  @IsOptional()
+  @IsOptional()
+  checkoutDate?: Date;
+
+  @IsDate()
+  @IsOptional()
+  @IsOptional()
+  departureDate?: Date;
+
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  @IsOptional()
+  peopleQuantity?: number;
+
+  @IsEnum(ReservationState)
+  state?: ReservationState;
+
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  @IsOptional()
+  total?: number;
+
+  @IsEnum(ReservationOrigin)
+  @IsOptional()
+  origin?: ReservationOrigin;
+
+  @IsNumber()
+  @IsPositive()
+  @Min(0)
+  @IsOptional()
   balance: number;
 }
