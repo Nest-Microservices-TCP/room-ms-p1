cf0d5fba7b20e1b4c9188c174315efc11d258127
JosueEC <josueev.cruz@gmail.com>
2024-11-16 20:37:14 -0600

    [FEAT](reservation.response): Define the dto properties

--- src/reservations/dto/response/reservation.response.dto.ts
+++ src/reservations/dto/response/reservation.response.dto.ts
@@ -1,1 +1,12 @@
-export class ReservationResponseDto {}
+import { ReservationOrigin, ReservationState } from 'src/reservations/enum';
+
+export class ReservationResponseDto {
+  reservation_id: string;
+  checkoutDate: Date;
+  departureDate: Date;
+  peopleQuantity: number;
+  state: ReservationState;
+  total: number;
+  origin: ReservationOrigin;
+  balance: number;
+}
