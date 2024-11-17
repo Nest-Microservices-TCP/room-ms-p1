747fc5f9428cd82a52779529e12a869c2630896c
JosueEC <josueev.cruz@gmail.com>
2024-11-16 20:38:52 -0600

    [FEAT](reservations.repository.interface): Extends the base-repository-interface

--- src/reservations/repository/interfaces/reservations.repository.interface.ts
+++ src/reservations/repository/interfaces/reservations.repository.interface.ts
@@ -1,1 +1,13 @@
-export interface IReservationsRepository {}
+import { ReservationEntity } from 'src/reservations/entity/reservation.entity';
+import { IBaseRepository } from 'src/common/interfaces';
+import {
+  UpdateReservationDto,
+  CreateReservationDto,
+} from 'src/reservations/dto/request';
+
+export interface IReservationsRepository
+  extends IBaseRepository<
+    ReservationEntity,
+    CreateReservationDto,
+    UpdateReservationDto
+  > {}
