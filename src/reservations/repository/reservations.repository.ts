f2bf397313feb07d9fd901067121fad5708c6efa
JosueEC <josueev.cruz@gmail.com>
2024-11-16 20:39:20 -0600

    [FEAT](reservations.repository): Implements the reservations-repository-interface

--- src/reservations/repository/reservations.repository.ts
+++ src/reservations/repository/reservations.repository.ts
@@ -1,1 +1,68 @@
-export class ReservationsRepository {}
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { IReservationsRepository } from './interfaces/reservations.repository.interface';
+import { CreateReservationDto, UpdateReservationDto } from '../dto/request';
+import { ReservationEntity } from '../entity/reservation.entity';
+import { DeleteResultResponse } from 'src/common/dto/response';
+import { QueryRunner, FindOptionsWhere } from 'typeorm';
+
+export class ReservationsRepository implements IReservationsRepository {
+  setQueryRunner(queryRunner: QueryRunner): void {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): Promise<ReservationEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  findOneById(id: string): Promise<ReservationEntity> {
+    throw new Error('Method not implemented.');
+  }
+  create(request: Partial<ReservationEntity>): ReservationEntity {
+    throw new Error('Method not implemented.');
+  }
+  save(request: CreateReservationDto): Promise<ReservationEntity> {
+    throw new Error('Method not implemented.');
+  }
+  update(request: UpdateReservationDto): Promise<ReservationEntity> {
+    throw new Error('Method not implemented.');
+  }
+  remove(id: string): Promise<DeleteResultResponse> {
+    throw new Error('Method not implemented.');
+  }
+  findByIds(ids: string[]): Promise<ReservationEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  findByCriteria(
+    criteria: FindOptionsWhere<ReservationEntity>,
+  ): Promise<ReservationEntity> {
+    throw new Error('Method not implemented.');
+  }
+  findWithRelations(relations: string[]): Promise<ReservationEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  count(criteria: FindOptionsWhere<ReservationEntity>): Promise<number> {
+    throw new Error('Method not implemented.');
+  }
+  paginate(
+    page: number,
+    limit: number,
+  ): Promise<[ReservationEntity[], number]> {
+    throw new Error('Method not implemented.');
+  }
+  softDelete(id: string): Promise<ReservationEntity> {
+    throw new Error('Method not implemented.');
+  }
+  restore(id: string): Promise<ReservationEntity> {
+    throw new Error('Method not implemented.');
+  }
+  exists(criteria: FindOptionsWhere<ReservationEntity>): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+  bulkSave(entities: ReservationEntity[]): Promise<ReservationEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  bulkUpdate(entities: ReservationEntity[]): Promise<ReservationEntity[]> {
+    throw new Error('Method not implemented.');
+  }
+  customQuery(query: string, params: any[]): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
