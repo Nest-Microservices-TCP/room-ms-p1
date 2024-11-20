87eaee5a8ffc9773034c9f197a02235d65469022
JosueEC <josueev.cruz@gmail.com>
2024-11-19 19:09:15 -0600

    [FEAT](reservations.service): Add the find-all method

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -1,9 +1,21 @@
+import { plainToInstance } from 'class-transformer';
+import { ReservationResponseDto } from './dto/response';
 import { ReservationsRepository } from './repository/reservations.repository';
 import { Injectable } from '@nestjs/common';
+import { HandleRpcExceptions } from 'src/common/decorators';
 
 @Injectable()
 export class ReservationsService {
   constructor(
     private readonly reservationsRepository: ReservationsRepository,
   ) {}
+
+  @HandleRpcExceptions()
+  async findAll(): Promise<ReservationResponseDto[]> {
+    const reservations = await this.reservationsRepository.findAll();
+
+    return plainToInstance(ReservationResponseDto, reservations, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
