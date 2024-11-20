cc9df019411743709a2c90e4e834a08dd762ab8d
JosueEC <josueev.cruz@gmail.com>
2024-11-20 17:18:38 -0600

    [REFACTOR](main.ts): Sort the imports sentences

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Rooms-MS');
