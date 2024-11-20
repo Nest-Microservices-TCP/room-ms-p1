661545ca01595feee7c1e46cb243f0d6970b451a
JosueEC <josueev.cruz@gmail.com>
2024-11-19 19:18:34 -0600

    [FEAT](reservations.controller): Expose the find-all endpoint

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -1,7 +1,14 @@
 import { Controller } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
+import { ReservationResponseDto } from './dto/response';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
+
+  @MessagePattern({ cmd: 'find.all.reservations' })
+  async findAll(): Promise<ReservationResponseDto[]> {
+    return this.reservationsService.findAll();
+  }
 }
