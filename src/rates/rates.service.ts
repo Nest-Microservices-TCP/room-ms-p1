3362061eee0bd7f797a2427a314cb91de2e3a335
JosueEC <josueev.cruz@gmail.com>
2024-11-23 19:24:17 -0600

    [FEAT](rates.service): Add the find-by-ids service method

--- src/rates/rates.service.ts
+++ src/rates/rates.service.ts
@@ -28,6 +28,15 @@ export class RatesService {
   }
 
   @HandleRpcExceptions()
+  async findByIds(ratesIds: string[]): Promise<RateResponseDto[]> {
+    const rates = await this.ratesRepository.findByIds(ratesIds);
+
+    return plainToInstance(RateResponseDto, rates, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  @HandleRpcExceptions()
   async save(request: CreateRateDto): Promise<RateResponseDto> {
     const rate = await this.ratesRepository.save(request);
 
