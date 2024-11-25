eb72c39d7947cc751727170714b1da72e59df10e
JosueEC <josueev.cruz@gmail.com>
2024-11-24 19:33:00 -0600

    [FEAT](rents-extras/dto/response): Add the rent-extra-response-dto

---
+++ src/rents-extras/dto/response/rent-extra.response.dto.ts
@@ -0,0 +1,20 @@
+import { ExtraEntity } from 'src/extras/entity/extra.entity';
+import { RentEntity } from 'src/rents/entity';
+import { Expose } from 'class-transformer';
+
+export class RentExtraResponseDto {
+  @Expose()
+  rentExtraId: string;
+
+  @Expose()
+  quantity: number;
+
+  @Expose()
+  total: number;
+
+  @Expose()
+  rent: RentEntity;
+
+  @Expose()
+  extra: ExtraEntity;
+}
