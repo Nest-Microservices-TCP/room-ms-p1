0040a4ada64df63da614c61eafd9944a6a85a900
JosueEC <josueev.cruz@gmail.com>
2024-11-27 19:35:12 -0600

    [FEAT](create-rent-extra.dto): Add validations for dto properties

--- src/rents-extras/dto/request/create-rent-extra.dto.ts
+++ src/rents-extras/dto/request/create-rent-extra.dto.ts
@@ -1,9 +1,20 @@
+import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
 import { ExtraEntity } from 'src/extras/entity/extra.entity';
 import { RentEntity } from 'src/rents/entity';
 
 export class CreateRentExtraDto {
+  @Min(1)
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   quantity: number;
+
+  @Min(1)
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   total: number;
+
   rent: RentEntity;
   extra: ExtraEntity;
 }
