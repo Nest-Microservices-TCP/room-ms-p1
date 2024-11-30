591a2d3f416a86ef3019943905798916ce123d92
JosueEC <josueev.cruz@gmail.com>
2024-11-29 21:59:32 -0600

    [FEAT](rents-extras.controller): Expose the find-all controller endpoint

--- src/rents-extras/rents-extras.controller.ts
+++ src/rents-extras/rents-extras.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
 import { RentsExtrasService } from './rents-extras.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { RentExtraResponseDto } from './dto/response';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class RentsExtrasController {
   constructor(private readonly rentsExtrasService: RentsExtrasService) {}
+
+  @MessagePattern({ cmd: 'find.all.rents.extras' })
+  async findAll(): Promise<RentExtraResponseDto[]> {
+    return this.rentsExtrasService.findAll();
+  }
 }
