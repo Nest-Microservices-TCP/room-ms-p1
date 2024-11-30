2984f9595e8b8bc814d8c1a3c2eaee250f7120b7
JosueEC <josueev.cruz@gmail.com>
2024-11-29 21:46:48 -0600

    [FEAT](rents-extras.service): Add the find-all service method

--- src/rents-extras/rents-extras.service.ts
+++ src/rents-extras/rents-extras.service.ts
@@ -1,7 +1,19 @@
+import { plainToInstance } from 'class-transformer';
+import { RentExtraResponseDto } from './dto/response';
 import { RentsExtrasRepository } from './repository/rents-extras.repository';
 import { Injectable } from '@nestjs/common';
+import { HandleRpcExceptions } from 'src/common/decorators';
 
 @Injectable()
 export class RentsExtrasService {
   constructor(private readonly rentsExtrasRepository: RentsExtrasRepository) {}
+
+  @HandleRpcExceptions()
+  async findAll(): Promise<RentExtraResponseDto[]> {
+    const rentsExtras = await this.rentsExtrasRepository.findAll();
+
+    return plainToInstance(RentExtraResponseDto, rentsExtras, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
