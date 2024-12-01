b2112352d7c832c935872267b6208aed5438c8a6
JosueEC <josueev.cruz@gmail.com>
2024-11-30 20:25:36 -0600

    [FEAT](amenity.entity): Add typeorm decorators and define config for postgres database

--- src/amenities/entity/amenity.entity.ts
+++ src/amenities/entity/amenity.entity.ts
@@ -1,8 +1,34 @@
 import { BaseEntity } from 'src/common/entity';
 
-export class Amenity extends BaseEntity {
-  amenity_id: string;
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity({ name: 'amenity' })
+export class AmenityEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'amenity_id',
+    primaryKeyConstraintName: 'PK_amenity',
+  })
+  amenityId: string;
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+  })
   name: string;
+
+  @Column({
+    name: 'quantity',
+    type: 'smallint',
+    nullable: false,
+  })
   quantity: number;
+
+  @Column({
+    name: 'description',
+    type: 'varchar',
+    length: 255,
+    nullable: true,
+  })
   description: string;
 }
