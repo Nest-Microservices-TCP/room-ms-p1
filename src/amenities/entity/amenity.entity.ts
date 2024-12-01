3c36f113fcaead9c31199d9e0d3c89c46dbaf38f
JosueEC <josueev.cruz@gmail.com>
2024-11-30 20:21:20 -0600

    [FEAT](amenity.entity): Define the entity properties

--- src/amenities/entity/amenity.entity.ts
+++ src/amenities/entity/amenity.entity.ts
@@ -1,1 +1,8 @@
-export class Amenity {}
+import { BaseEntity } from 'src/common/entity';
+
+export class Amenity extends BaseEntity {
+  amenity_id: string;
+  name: string;
+  quantity: number;
+  description: string;
+}
