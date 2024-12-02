763511b3fee23d4583650b1c58877e0fe1fdc819
JosueEC <josueev.cruz@gmail.com>
2024-12-01 19:41:17 -0600

    [FEAT](amenities.repository.interface): Extends the interface from the base-repository-interface

--- src/amenities/repository/interfaces/amenities.repository.interface.ts
+++ src/amenities/repository/interfaces/amenities.repository.interface.ts
@@ -1,1 +1,6 @@
-export interface IAmenitiesRepository {}
+import { CreateAmenityDto, UpdateAmenityDto } from 'src/amenities/dto/request';
+import { AmenityEntity } from 'src/amenities/entity/amenity.entity';
+import { IBaseRepository } from 'src/common/interfaces';
+
+export interface IAmenitiesRepository
+  extends IBaseRepository<AmenityEntity, CreateAmenityDto, UpdateAmenityDto> {}
