dcee7160fc41c513a6a927f6ef192451d07f55a0
JosueEC <josueev.cruz@gmail.com>
2024-12-03 19:14:40 -0600

    [FEAT](reservations.module): Inject the modules for imports, providers and controllers arrays

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -1,4 +1,13 @@
+import { ReservationsRepository } from './repository/reservations.repository';
+import { ReservationsController } from './reservations.controller';
+import { ReservationEntity } from './entity/reservation.entity';
+import { ReservationsService } from './reservations.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([ReservationEntity])],
+  providers: [ReservationsRepository, ReservationsService],
+  controllers: [ReservationsController],
+})
 export class ReservationsModule {}
