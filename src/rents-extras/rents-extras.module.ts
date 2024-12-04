caadaad3c5690b1ed8ec4f10c4d51891e86b3dd0
JosueEC <josueev.cruz@gmail.com>
2024-12-03 19:07:50 -0600

    [FEAT](rents-extras.module): Inject the providers and controllers and config de module for typeorm feature

--- src/rents-extras/rents-extras.module.ts
+++ src/rents-extras/rents-extras.module.ts
@@ -1,4 +1,13 @@
+import { RentsExtrasRepository } from './repository/rents-extras.repository';
+import { RentsExtrasController } from './rents-extras.controller';
+import { RentExtraEntity } from './entity/rent-extra.entity';
+import { RentsExtrasService } from './rents-extras.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([RentExtraEntity])],
+  providers: [RentsExtrasRepository, RentsExtrasService],
+  controllers: [RentsExtrasController],
+})
 export class RentsExtrasModule {}
