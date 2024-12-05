f97a7b13abe31494aa7ae3637b05bb9c28a8b322
JosueEC <josueev.cruz@gmail.com>
2024-12-04 19:27:09 -0600

    [FEAT](amenities.service): Add the find-all service method

--- src/amenities/amenities.service.ts
+++ src/amenities/amenities.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { AmenitiesRepository } from './repository/amenities.repository';
+import { AmenityResponseDto } from './dto/response';
+import { plainToInstance } from 'class-transformer';
+import { HandleRpcExceptions } from 'src/common/decorators';
 
 @Injectable()
 export class AmenitiesService {
   constructor(private readonly amenitiesRepository: AmenitiesRepository) {}
+
+  @HandleRpcExceptions()
+  async findAll(): Promise<AmenityResponseDto[]> {
+    const amenities = await this.amenitiesRepository.findAll();
+
+    return plainToInstance(AmenityResponseDto, amenities, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
