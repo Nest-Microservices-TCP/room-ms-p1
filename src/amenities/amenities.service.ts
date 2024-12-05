9a97dda5a63deff44b71dd55581d0df973aff9a0
JosueEC <josueev.cruz@gmail.com>
2024-12-04 19:25:43 -0600

    [FEAT](amenities.service): Inject the amenities-repository to the class

--- src/amenities/amenities.service.ts
+++ src/amenities/amenities.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { AmenitiesRepository } from './repository/amenities.repository';
 
 @Injectable()
-export class AmenitiesService {}
+export class AmenitiesService {
+  constructor(private readonly amenitiesRepository: AmenitiesRepository) {}
+}
