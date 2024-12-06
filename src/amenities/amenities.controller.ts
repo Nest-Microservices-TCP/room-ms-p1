6b333cc17b315f2da219316a408c0606f56ab501
JosueEC <josueev.cruz@gmail.com>
2024-12-05 19:46:19 -0600

    [FEAT](amenities.controller): Expose the find-all controller endpoint

--- src/amenities/amenities.controller.ts
+++ src/amenities/amenities.controller.ts
@@ -1,7 +1,14 @@
 import { Controller } from '@nestjs/common';
 import { AmenitiesService } from './amenities.service';
+import { AmenityResponseDto } from './dto/response';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AmenitiesController {
   constructor(private readonly amenitiesService: AmenitiesService) {}
+
+  @MessagePattern({ cmd: 'find.all.amenities' })
+  async findAll(): Promise<AmenityResponseDto[]> {
+    return this.amenitiesService.findAll();
+  }
 }
