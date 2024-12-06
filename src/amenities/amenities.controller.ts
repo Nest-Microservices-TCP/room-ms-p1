bdfb72791bfa43c60a7c50885d738d8f395bf7c1
JosueEC <josueev.cruz@gmail.com>
2024-12-05 19:45:24 -0600

    [FEAT](amenities.controller): Inject the amenities-service to the class

--- src/amenities/amenities.controller.ts
+++ src/amenities/amenities.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { AmenitiesService } from './amenities.service';
 
 @Controller()
-export class AmenitiesController {}
+export class AmenitiesController {
+  constructor(private readonly amenitiesService: AmenitiesService) {}
+}
