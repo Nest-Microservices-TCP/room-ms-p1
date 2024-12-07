fe35c128f157958e164b6de51fbc068618b0537b
JosueEC <josueev.cruz@gmail.com>
2024-12-06 22:23:20 -0600

    [FEAT](extras.module): Inject the repository, service, controller and typeorm configuration for arrays extras module

--- src/extras/extras.module.ts
+++ src/extras/extras.module.ts
@@ -1,4 +1,13 @@
+import { ExtrasRepository } from './repository/extras.repository';
+import { ExtrasController } from './extras.controller';
+import { ExtrasService } from './extras.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Extra } from './entity/extra.entity';
 import { Module } from '@nestjs/common';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Extra])],
+  providers: [ExtrasRepository, ExtrasService],
+  controllers: [ExtrasController],
+})
 export class ExtrasModule {}
