dd54921a946e09b3daa173cbe3800b5a3d7a1d75
JosueEC <josueev.cruz@gmail.com>
2024-12-06 22:07:21 -0600

    [FEAT](amenities.module): Inject the repository, service controller and typeorm configuration to the arrays module

--- src/amenities/amenities.module.ts
+++ src/amenities/amenities.module.ts
@@ -1,4 +1,13 @@
+import { AmenitiesRepository } from './repository/amenities.repository';
+import { AmenitiesController } from './amenities.controller';
+import { AmenityEntity } from './entity/amenity.entity';
+import { AmenitiesService } from './amenities.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([AmenityEntity])],
+  providers: [AmenitiesRepository, AmenitiesService],
+  controllers: [AmenitiesController],
+})
 export class AmenitiesModule {}
