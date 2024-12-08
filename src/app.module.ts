410aab5f48054a566f7d91c59aeb3e28a13e6aa5
JosueEC <josueev.cruz@gmail.com>
2024-12-07 20:05:57 -0600

    [FEAT](app.module): Inject the extras-module to the imports array

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,19 @@ import { PostgresConfigModule } from './database/postgres.config.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { RentsExtrasModule } from './rents-extras/rents-extras.module';
 import { RoomsStatesModule } from './rooms-states/rooms-states.module';
+import { AmenitiesModule } from './amenities/amenities.module';
+import { ExtrasModule } from './extras/extras.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { RatesModule } from './rates/rates.module';
 import { RentsModule } from './rents/rents.module';
 import { Module } from '@nestjs/common';
-import { AmenitiesModule } from './amenities/amenities.module';
 
 @Module({
   imports: [
     RoomsModule,
     RentsModule,
     RatesModule,
+    ExtrasModule,
     AmenitiesModule,
     RentsExtrasModule,
     RoomsStatesModule,
