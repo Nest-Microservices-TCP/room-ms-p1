5d1bbc1922b3dc045b9ac0441c03acdcdb2a9797
JosueEC <josueev.cruz@gmail.com>
2024-12-07 20:09:05 -0600

    [REFACTOR](base-repository.interface): Rename the find-one-by-id method bt just find-one

--- src/common/interfaces/base-repository.interface.ts
+++ src/common/interfaces/base-repository.interface.ts
@@ -4,7 +4,7 @@ import { DeleteResultResponse } from '../dto/response';
 export interface IBaseRepository<T, CreateDto, UpdateDto> {
   setQueryRunner(queryRunner: QueryRunner): void;
   findAll(): Promise<T[]>;
-  findOneById(id: string): Promise<T>;
+  findOne(id: string): Promise<T>;
   create(request: Partial<T>): T;
   save(request: CreateDto): Promise<T>;
   update(request: UpdateDto): Promise<T>;
