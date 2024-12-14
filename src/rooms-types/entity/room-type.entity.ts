bd85c04b12ae85454fbd24e69ce124368a54738b
JosueEC <josueev.cruz@gmail.com>
2024-12-13 20:42:18 -0600

    [FEAT](room-type.entity): Add properties database decorators

--- src/rooms-types/entity/room-type.entity.ts
+++ src/rooms-types/entity/room-type.entity.ts
@@ -1,6 +1,33 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'rooms_types' })
 export class RoomType {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'room_type_id',
+    primaryKeyConstraintName: 'PK_rooms_types',
+  })
   roomTypeId: string;
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+    unique: true,
+  })
   name: string;
+
+  @Column({
+    name: 'description',
+    type: 'text',
+    nullable: true,
+  })
   description: string;
+
+  @Column({
+    name: 'included_people',
+    type: 'smallint',
+    nullable: true,
+  })
   includedPeople: number;
 }
