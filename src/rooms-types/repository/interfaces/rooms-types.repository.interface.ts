9388336beb1cb6ad2c2830cb32bd85137dcaf18a
JosueEC <josueev.cruz@gmail.com>
2024-12-14 20:16:39 -0600

    [FEAT](rooms-types.repository.interface): Extends from the base-repository-interface and pass the entity and dtos for the generic interface

--- src/rooms-types/repository/interfaces/rooms-types.repository.interface.ts
+++ src/rooms-types/repository/interfaces/rooms-types.repository.interface.ts
@@ -1,1 +1,9 @@
-export interface IRoomsTypesRepository {}
+import { RoomType } from 'src/rooms-types/entity/room-type.entity';
+import { IBaseRepository } from 'src/common/interfaces';
+import {
+  CreateRoomTypeDto,
+  UpdateRoomTypeDto,
+} from 'src/rooms-types/dto/request';
+
+export interface IRoomsTypesRepository
+  extends IBaseRepository<RoomType, CreateRoomTypeDto, UpdateRoomTypeDto> {}
