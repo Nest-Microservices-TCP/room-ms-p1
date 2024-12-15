b958080227e136cb7749ece86d968748c972f8da
JosueEC <josueev.cruz@gmail.com>
2024-12-14 20:14:04 -0600

    [FEAT](room-type.response.dto): Add the Expose decorator for the room-type-response properties

--- src/rooms-types/dto/response/room-type.response.dto.ts
+++ src/rooms-types/dto/response/room-type.response.dto.ts
@@ -1,6 +1,15 @@
+import { Expose } from 'class-transformer';
+
 export class RoomTypeResponseDto {
+  @Expose()
   roomTypeId: string;
+
+  @Expose()
   name: string;
+
+  @Expose()
   description: string;
+
+  @Expose()
   includedPeople: number;
 }
