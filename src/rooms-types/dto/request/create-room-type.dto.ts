8ee102aca7df4e2d7eab0fa7436181004af26464
JosueEC <josueev.cruz@gmail.com>
2024-12-14 20:10:12 -0600

    [FEAT](create-room-type.dto): Add validators for the dto properties

--- src/rooms-types/dto/request/create-room-type.dto.ts
+++ src/rooms-types/dto/request/create-room-type.dto.ts
@@ -1,5 +1,26 @@
+import {
+  IsNumber,
+  IsString,
+  MaxLength,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+} from 'class-validator';
+
 export class CreateRoomTypeDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @IsOptional()
   description?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   includedPeople?: number;
 }
