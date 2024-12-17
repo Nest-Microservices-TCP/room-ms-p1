24a0c3be768420e4659fe97dfde332a59592ae96
JosueEC <josueev.cruz@gmail.com>
2024-12-16 18:19:43 -0600

    [FEAT](rooms-types.service): Inject the rooms-types-repository to the service class

--- src/rooms-types/rooms-types.service.ts
+++ src/rooms-types/rooms-types.service.ts
@@ -1,1 +1,7 @@
-export class RoomsTypesService {}
+import { RoomsTypesRepository } from './repository/rooms-types.repository';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RoomsTypesService {
+  constructor(private readonly roomsTypesRepository: RoomsTypesRepository) {}
+}
