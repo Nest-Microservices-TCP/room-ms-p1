3e3a51012891989007c4d3a294d41916e97fe323
JosueEC <josueev.cruz@gmail.com>
2024-12-17 18:16:16 -0600

    [REFACTOR](rooms-types.service): Use the plain-to-instance-dto class method for all service methods

--- src/rooms-types/rooms-types.service.ts
+++ src/rooms-types/rooms-types.service.ts
@@ -20,45 +20,35 @@ export class RoomsTypesService {
   async findAll(): Promise<RoomTypeResponseDto[]> {
     const roomsTypes = await this.roomsTypesRepository.findAll();
 
-    return plainToInstance(RoomTypeResponseDto, roomsTypes, {
-      excludeExtraneousValues: true,
-    });
+    return this.plainToInstanceDto(roomsTypes);
   }
 
   @HandleRpcExceptions()
   async findOne(roomTypeId: string): Promise<RoomTypeResponseDto> {
     const roomType = await this.roomsTypesRepository.findOne(roomTypeId);
 
-    return plainToInstance(RoomTypeResponseDto, roomType, {
-      excludeExtraneousValues: true,
-    });
+    return this.plainToInstanceDto(roomType);
   }
 
   @HandleRpcExceptions()
   async findByIds(roomsTypesIds: string[]): Promise<RoomTypeResponseDto[]> {
     const roomsTypes = await this.roomsTypesRepository.findByIds(roomsTypesIds);
 
-    return plainToInstance(RoomTypeResponseDto, roomsTypes, {
-      excludeExtraneousValues: true,
-    });
+    return this.plainToInstanceDto(roomsTypes);
   }
 
   @HandleRpcExceptions()
   async save(request: CreateRoomTypeDto): Promise<RoomTypeResponseDto> {
     const newRoomType = await this.roomsTypesRepository.save(request);
 
-    return plainToInstance(RoomTypeResponseDto, newRoomType, {
-      excludeExtraneousValues: true,
-    });
+    return this.plainToInstanceDto(newRoomType);
   }
 
   @HandleRpcExceptions()
   async update(request: UpdateRoomTypeDto): Promise<RoomTypeResponseDto> {
     const roomTypeUpdated = await this.roomsTypesRepository.update(request);
 
-    return plainToInstance(RoomTypeResponseDto, roomTypeUpdated, {
-      excludeExtraneousValues: true,
-    });
+    return this.plainToInstanceDto(roomTypeUpdated);
   }
 
   @HandleRpcExceptions()
