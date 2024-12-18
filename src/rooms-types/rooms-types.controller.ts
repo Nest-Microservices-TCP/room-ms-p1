068449be9d4b3b63f5aac160455f9f66f85cfc3e
JosueEC <josueev.cruz@gmail.com>
2024-12-17 18:18:31 -0600

    [FEAT](rooms-types.controller): Expose the find-all controller endpoint

--- src/rooms-types/rooms-types.controller.ts
+++ src/rooms-types/rooms-types.controller.ts
@@ -1,7 +1,14 @@
 import { RoomsTypesService } from './rooms-types.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { RoomTypeResponseDto } from './dto/response';
 import { Controller } from '@nestjs/common';
 
 @Controller()
 export class RoomsTypesController {
   constructor(private readonly roomsTypesService: RoomsTypesService) {}
+
+  @MessagePattern({ cmd: 'find.all.rooms.types' })
+  findAll(): Promise<RoomTypeResponseDto[]> {
+    return this.roomsTypesService.findAll();
+  }
 }
