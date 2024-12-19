35f82b9345e5bb791e94fc3020f3c64f3107a697
JosueEC <josueev.cruz@gmail.com>
2024-12-18 19:25:06 -0600

    [FEAT](rooms-types.module): Inject the dependencies for the rooms-types-module

--- src/rooms-types/rooms-types.module.ts
+++ src/rooms-types/rooms-types.module.ts
@@ -1,1 +1,13 @@
+import { RoomsTypesRepository } from './repository/rooms-types.repository';
+import { RoomsTypesController } from './rooms-types.controller';
+import { RoomsTypesService } from './rooms-types.service';
+import { RoomType } from './entity/room-type.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([RoomType])],
+  providers: [RoomsTypesRepository, RoomsTypesService],
+  controllers: [RoomsTypesController],
+})
 export class RoomsTypesModule {}
