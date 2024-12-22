db677226bb123d33143c13147f0c996089e9c496
JosueEC <josueev.cruz@gmail.com>
2024-12-21 19:42:01 -0600

    [FEAT](reservation-state.entity): Define the properties and column configurations for the database

--- src/reservations-states/entity/reservation-state.entity.ts
+++ src/reservations-states/entity/reservation-state.entity.ts
@@ -1,1 +1,26 @@
-export class ReservationState {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'reservations-states' })
+export class ReservationState {
+  @PrimaryGeneratedColumn('uuid', {
+    primaryKeyConstraintName: 'PK_reservation_state_id',
+    name: 'reservation_state_id',
+  })
+  reservationStateId: string;
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    unique: true,
+  })
+  name: string;
+
+  @Column({
+    name: 'description',
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+}
