7a886c64add2145acc82cf62989eceb41d9ac854
JosueEC <josueev.cruz@gmail.com>
2024-12-21 19:44:15 -0600

    [FEAT](create-reservation-state.dto): Define the properties and column validations with class-validator

--- src/reservations-states/dto/request/create-reservation-state.dto.ts
+++ src/reservations-states/dto/request/create-reservation-state.dto.ts
@@ -1,1 +1,13 @@
-export class CreateReservationStateDto {}
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateReservationStateDto {
+  @IsString()
+  @MaxLength(255)
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  description?: string;
+}
