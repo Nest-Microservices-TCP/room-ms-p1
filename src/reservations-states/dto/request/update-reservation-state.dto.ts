b43f66656ca1de9bb108becbdf27c23e3f636c47
JosueEC <josueev.cruz@gmail.com>
2024-12-21 19:46:29 -0600

    [FEAT](update-reservation-state.dto): Define the properties and validations with class-validator

--- src/reservations-states/dto/request/update-reservation-state.dto.ts
+++ src/reservations-states/dto/request/update-reservation-state.dto.ts
@@ -1,1 +1,23 @@
-export class UpdateReservationStateDto {}
+import {
+  IsUUID,
+  IsString,
+  MaxLength,
+  IsOptional,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class UpdateReservationStateDto {
+  @IsUUID()
+  reservationStateId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  description?: string;
+}
