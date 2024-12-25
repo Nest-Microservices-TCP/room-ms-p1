8ea1541cdfea7e25c435edb7b39d4e1f46794cfe
JosueEC <josueev.cruz@gmail.com>
2024-12-25 11:14:14 -0600

    [FEAT](reservations-states.controller): Inject the reservations-states-service to the constructor class

--- src/reservations-states/reservations-states.controller.ts
+++ src/reservations-states/reservations-states.controller.ts
@@ -1,1 +1,9 @@
-export class ReservationsStatesController {}
+import { ReservationsStatesService } from './reservations-states.service';
+import { Controller } from '@nestjs/common';
+
+@Controller()
+export class ReservationsStatesController {
+  constructor(
+    private readonly reservationsStatesService: ReservationsStatesService,
+  ) {}
+}
