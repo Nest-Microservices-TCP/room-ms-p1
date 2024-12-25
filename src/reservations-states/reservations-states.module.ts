5b1aef17a72ef349d501c5781d19590795eba79d
JosueEC <josueev.cruz@gmail.com>
2024-12-25 11:24:01 -0600

    [FEAT](reservations-states.module): Inject the service, repository and controller files for the reservations-states-module

--- src/reservations-states/reservations-states.module.ts
+++ src/reservations-states/reservations-states.module.ts
@@ -1,8 +1,13 @@
+import { ReservationsStatesRepository } from './repository/reservations-states.repository';
+import { ReservationsStatesController } from './reservations-states.controller';
+import { ReservationsStatesService } from './reservations-states.service';
 import { ReservationState } from './entity/reservation-state.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ReservationState])],
+  providers: [ReservationsStatesRepository, ReservationsStatesService],
+  controllers: [ReservationsStatesController],
 })
 export class ReservationsStatesModule {}
