9c239ccf3bfe82a579378fa1e98cc24260ee92d8
JosueEC <josueev.cruz@gmail.com>
2024-12-25 11:22:54 -0600

    [FEAT](reservations-states.module): Inject the typeorm config for the reservation-state-entity

--- src/reservations-states/reservations-states.module.ts
+++ src/reservations-states/reservations-states.module.ts
@@ -1,1 +1,8 @@
+import { ReservationState } from './entity/reservation-state.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ReservationState])],
+})
 export class ReservationsStatesModule {}
