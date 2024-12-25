5735b0b723e2247d49cd9c880072b763c942e35f
JosueEC <josueev.cruz@gmail.com>
2024-12-25 11:16:33 -0600

    [FEAT](reservations-states.controller): Expose the find-one controller endpoint

--- src/reservations-states/reservations-states.controller.ts
+++ src/reservations-states/reservations-states.controller.ts
@@ -1,6 +1,6 @@
 import { ReservationsStatesService } from './reservations-states.service';
 import { ReservationStateResponseDto } from './dto/response';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Controller } from '@nestjs/common';
 
 @Controller()
@@ -12,5 +12,12 @@ export class ReservationsStatesController {
   @MessagePattern({ cmd: 'find.all.reservations.states' })
   findAll(): Promise<ReservationStateResponseDto[]> {
     return this.reservationsStatesService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'find.one.reservation.state' })
+  findOne(
+    @Payload('reservationStateId') reservationStateId: string,
+  ): Promise<ReservationStateResponseDto> {
+    return this.reservationsStatesService.findOne(reservationStateId);
   }
 }
