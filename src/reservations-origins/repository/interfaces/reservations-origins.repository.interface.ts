4bf7c7f14291c589d25fd00acdfb88ee8bbafc49
JosueEC <josueev.cruz@gmail.com>
2024-12-29 19:45:27 -0600

    [FEAT](reservations-origins.repository.interface): Extends from the base-repository-interface

--- src/reservations-origins/repository/interfaces/reservations-origins.repository.interface.ts
+++ src/reservations-origins/repository/interfaces/reservations-origins.repository.interface.ts
@@ -1,1 +1,13 @@
-export interface IReservationsOriginsRepository {}
+import { ReservationOrigin } from 'src/reservations-origins/entity/reservation-origin.entity';
+import { IBaseRepository } from 'src/common/interfaces';
+import {
+  CreateReservationOriginDto,
+  UpdateReservationOriginDto,
+} from 'src/reservations-origins/dto/request';
+
+export interface IReservationsOriginsRepository
+  extends IBaseRepository<
+    ReservationOrigin,
+    CreateReservationOriginDto,
+    UpdateReservationOriginDto
+  > {}
