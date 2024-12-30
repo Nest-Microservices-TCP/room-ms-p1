623260de72619b21f65df4d259cc4cdc01c85b24
JosueEC <josueev.cruz@gmail.com>
2024-12-29 19:37:27 -0600

    [FEAT](reservation-origin.entity): Define the entity properties and column configuration for typeorm

--- src/reservations-origins/entity/reservation-origin.entity.ts
+++ src/reservations-origins/entity/reservation-origin.entity.ts
@@ -1,1 +1,26 @@
-export class ReservationOrigin {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'reservations_origins' })
+export class ReservationOrigin {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'reservation_origin_id',
+    primaryKeyConstraintName: 'PK_reservations_origins',
+  })
+  reservationOriginId: string;
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+    unique: true,
+  })
+  name: string;
+
+  @Column({
+    name: 'description',
+    type: 'text',
+    nullable: true,
+  })
+  description?: string;
+}
