448921ba0c42008408a9272c58050e73728e91b7
JosueEC <josueev.cruz@gmail.com>
2024-12-29 19:40:25 -0600

    [FEAT](reservations-origins/dto/request): Define the properties for the create-reservation-origin dto and add validations from class-validator

--- src/reservations-origins/dto/request/create-reservation-origin.dto.ts
+++ src/reservations-origins/dto/request/create-reservation-origin.dto.ts
@@ -1,1 +1,13 @@
-export class CreateReservationOriginDto {}
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateReservationOriginDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  description?: string;
+}
