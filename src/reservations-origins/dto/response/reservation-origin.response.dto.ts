1dd5f32cef81a28ee964ce9c9f1fd0d282aef4d3
JosueEC <josueev.cruz@gmail.com>
2024-12-29 19:44:09 -0600

    [FEAT](reservation-origin,response): Define the dto properties and expose it with using class-transformer

--- src/reservations-origins/dto/response/reservation-origin.response.dto.ts
+++ src/reservations-origins/dto/response/reservation-origin.response.dto.ts
@@ -1,1 +1,12 @@
-export class ReservationOriginResponseDto {}
+import { Expose } from 'class-transformer';
+
+export class ReservationOriginResponseDto {
+  @Expose()
+  reservationOriginId: string;
+
+  @Expose()
+  name: string;
+
+  @Expose()
+  description: string;
+}
