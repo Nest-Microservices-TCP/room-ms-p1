1609f53fb0256b293785861c4001db9a9cf94e75
JosueEC <josueev.cruz@gmail.com>
2024-12-29 19:42:27 -0600

    [FEAT](reservations-origins/dto/request): Define the properties for the update-reservation-origin dto and add validations from class-validator

--- src/reservations-origins/dto/request/update-reservation-origin.dto.ts
+++ src/reservations-origins/dto/request/update-reservation-origin.dto.ts
@@ -1,1 +1,23 @@
-export class UpdateReservationOriginDto {}
+import {
+  IsUUID,
+  IsString,
+  MaxLength,
+  IsNotEmpty,
+  IsOptional,
+} from 'class-validator';
+
+export class UpdateReservationOriginDto {
+  @IsUUID()
+  reservationOriginId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  description?: string;
+}
