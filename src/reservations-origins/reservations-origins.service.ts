b66330193e70ecc8dfef86995ea286b5281fc8db
JosueEC <josueev.cruz@gmail.com>
2024-12-31 17:38:50 -0600

    [FEAT](reservations-origins.service): Inject the reservations-origins-repository to the class

--- src/reservations-origins/reservations-origins.service.ts
+++ src/reservations-origins/reservations-origins.service.ts
@@ -1,1 +1,9 @@
-export class ReservationsOriginsService {}
+import { ReservationsOriginsRepository } from './repository/reservations-origins.repository';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ReservationsOriginsService {
+  constructor(
+    private readonly reservationsOriginsRepository: ReservationsOriginsRepository,
+  ) {}
+}
