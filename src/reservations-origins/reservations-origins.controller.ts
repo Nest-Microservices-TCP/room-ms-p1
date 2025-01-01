0458c96e5095764a02a83a2ace311061a2b562b7
JosueEC <josueev.cruz@gmail.com>
2025-01-01 12:14:58 -0600

    [FEAT](reservations-origins.controller): Inject the reservations-origins-service to the class

--- src/reservations-origins/reservations-origins.controller.ts
+++ src/reservations-origins/reservations-origins.controller.ts
@@ -1,1 +1,9 @@
-export class ReservationsOriginsController {}
+import { ReservationsOriginsService } from './reservations-origins.service';
+import { Controller } from '@nestjs/common';
+
+@Controller()
+export class ReservationsOriginsController {
+  constructor(
+    private readonly reservationsOriginsService: ReservationsOriginsService,
+  ) {}
+}
