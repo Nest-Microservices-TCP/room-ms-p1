e2e54e6f199b234999928088ad2c499eac374ca0
JosueEC <josueev.cruz@gmail.com>
2025-01-02 17:33:52 -0600

    [FEAT](reservations-origins.module): Inject the module dependencies and the feature config for typeorm

--- src/reservations-origins/reservations-origins.module.ts
+++ src/reservations-origins/reservations-origins.module.ts
@@ -1,1 +1,13 @@
+import { ReservationsOriginsRepository } from './repository/reservations-origins.repository';
+import { ReservationsOriginsController } from './reservations-origins.controller';
+import { ReservationsOriginsService } from './reservations-origins.service';
+import { ReservationOrigin } from './entity/reservation-origin.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ReservationOrigin])],
+  providers: [ReservationsOriginsRepository, ReservationsOriginsService],
+  controllers: [ReservationsOriginsController],
+})
 export class ReservationsOriginsModule {}
