3d6824d3abbbd489d2c3688e0eb7becdffa4dbf8
JosueEC <josueev.cruz@gmail.com>
2025-01-04 19:27:20 -0600

    [FEAT](room.entity): Add the relation one-to-one with room-state-entity

--- src/rooms/entity/room.entity.ts
+++ src/rooms/entity/room.entity.ts
@@ -1,7 +1,14 @@
 import { BaseEntity } from 'src/common/entity';
 import { Rent } from 'src/rents/entity/rent.entity';
+import { RoomState } from 'src/rooms-states/entity/room-state.entity';
 
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity({ name: 'rooms' })
 export class Room extends BaseEntity {
@@ -20,4 +27,28 @@ export class Room extends BaseEntity {
 
   @OneToOne(() => Rent, (rent) => rent.room)
   rent: Rent;
+
+  @OneToOne(() => RoomState, (roomState) => roomState.room)
+  @JoinColumn({
+    /**
+     * @name Establece el nombre que tendrá la columna que almacenara
+     * la clave foránea de la relación
+     *
+     * @referencesColumnName Define el nombre de la columna sobre la
+     * cual se hará la referencia de la clave foránea de la otra tabla
+     * de la relación
+     *
+     * @foreignKeyConstraintName Define el nombre de la constraint que
+     * se genera para la clave foránea, si no se define, TypeORM genera
+     * una de forma automática
+     *
+     * ! NOTA: La entidad que tenga el decorador @JoinColumn es la que
+     * ! almacenara la clave foránea de forma física. En una relación
+     * ! @ManyToMany ambas tablas tienen el @JoinColumn
+     */
+    name: 'room_state_id',
+    referencedColumnName: 'room_state_id',
+    foreignKeyConstraintName: 'FK_Room_RoomState',
+  })
+  roomState: RoomState;
 }
