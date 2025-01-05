a7fb158d01ffbb69125d32e255959881b86f02cd
JosueEC <josueev.cruz@gmail.com>
2025-01-04 20:05:03 -0600

    [DOCS](room-type-amenity.entity): Add documentation for the typeorm relations configuration

--- src/rooms-types-amenities/entity/room-type-amenity.entity.ts
+++ src/rooms-types-amenities/entity/room-type-amenity.entity.ts
@@ -9,6 +9,16 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
+/**
+ * Cuando una tabla intermedia contiene otros campos a parte de las
+ * claves foráneas de la relación es prácticamente obligatorio crear
+ * una entidad a parte para esta tabla dentro de su propio modulo.
+ *
+ * Y por ende, ahora para establecer la relación @ManyToMany entre
+ * room-type y amenity se usa una relación @OneToMany de la entidades
+ * hacia la tabla intermedia y @ManyToOne de la tabla intermedia hacia
+ * las entidades
+ */
 @Entity({ name: 'rooms_types_amenities' })
 export class RoomTypeAmenity {
   @PrimaryGeneratedColumn('uuid', {
@@ -26,6 +36,21 @@ export class RoomTypeAmenity {
   amenityQuantity: number;
 
   @ManyToOne(() => RoomType, (roomType) => roomType.roomsTypesAmenities, {
+    /**
+     * * onDelete
+     * Si se elimina un registro en RoomType, la base de datos automáticamente
+     * elimina todas las relaciones asociadas en esta tabla intermedia sin
+     * necesidad de definir esta lógica de eliminación en el código
+     *
+     * ! Nota:
+     * La propiedad onDelete: 'CASCADE' siempre debe estar definida
+     * en los registros que estén asociados a la entidad principal, asi se
+     * mantiene la integridad referencial. Es decir, aquí nos importa que
+     * si ya no existe la entidad principal (RoomType) sus relaciones se
+     * eliminen para mantener la integridad, no al revés, si una relación
+     * se elimina no necesariamente queremos que se elimine la entidad
+     * principal
+     */
     onDelete: 'CASCADE',
   })
   @JoinColumn({
