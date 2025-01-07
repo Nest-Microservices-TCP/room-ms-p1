c20c91d63019a602e82cf6c682e83b5c85a935a5
JosueEC <josueev.cruz@gmail.com>
2025-01-06 20:49:18 -0600

    [FIX](rates.repository.interface): Extends from the repository-interface instead the base-repository-interface

--- src/rates/repository/interfaces/rates.repository.interface.ts
+++ src/rates/repository/interfaces/rates.repository.interface.ts
@@ -1,6 +1,6 @@
+import { IRepository } from 'src/common/repository';
 import { CreateRateDto, UpdateRateDto } from 'src/rates/dto/request';
-import { IBaseRepository } from 'src/common/interfaces';
 import { Rate } from 'src/rates/entity/rate.entity';
 
 export interface IRatesRepository
-  extends IBaseRepository<Rate, CreateRateDto, UpdateRateDto> {}
+  extends IRepository<Rate, CreateRateDto, UpdateRateDto> {}
