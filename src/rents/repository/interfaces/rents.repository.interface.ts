e0d3abd8b7a46eee3e0338630b5e7e201be5650e
JosueEC <josueev.cruz@gmail.com>
2025-01-06 20:49:30 -0600

    [FIX](rents.repository.interface): Extends from the repository-interface instead the base-repository-interface

--- src/rents/repository/interfaces/rents.repository.interface.ts
+++ src/rents/repository/interfaces/rents.repository.interface.ts
@@ -1,6 +1,6 @@
+import { IRepository } from 'src/common/repository';
 import { CreateRentDto, UpdateRentDto } from 'src/rents/dto/request';
-import { IBaseRepository } from 'src/common/interfaces';
 import { Rent } from 'src/rents/entity';
 
 export interface IRentsRepository
-  extends IBaseRepository<Rent, CreateRentDto, UpdateRentDto> {}
+  extends IRepository<Rent, CreateRentDto, UpdateRentDto> {}
