220e5bf3d254ada3b0a909f9cf6776fad88a310c
JosueEC <josueev.cruz@gmail.com>
2025-01-06 20:50:39 -0600

    [FIX](rooms.repository.interface): Extends from the repository-interface instead the base-repository-interface

--- src/rooms/repositories/interfaces/rooms.repository.interface.ts
+++ src/rooms/repositories/interfaces/rooms.repository.interface.ts
@@ -1,6 +1,6 @@
+import { IRepository } from 'src/common/repository';
 import { CreateRoomDto, UpdateRoomDto } from 'src/rooms/dto/request';
-import { IBaseRepository } from 'src/common/interfaces';
 import { Room } from 'src/rooms/entity/room.entity';
 
 export interface IRoomsRepository
-  extends IBaseRepository<Room, CreateRoomDto, UpdateRoomDto> {}
+  extends IRepository<Room, CreateRoomDto, UpdateRoomDto> {}
