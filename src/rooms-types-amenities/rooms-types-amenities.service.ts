880077ef5274b0cddf7eefd2f29173048ec6ab05
JosueEC <josueev.cruz@gmail.com>
2025-01-08 18:55:57 -0600

    [FEAT](rooms-types-amenities.service): Add the plain-to-instance-dto service method

--- src/rooms-types-amenities/rooms-types-amenities.service.ts
+++ src/rooms-types-amenities/rooms-types-amenities.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 
 import { RoomsTypesAmenitiesRepository } from './repository/rooms-types-amenities.repository';
+
+import { RoomTypeAmenityResponseDto } from './dto/response/room-type-amenity.response.dto';
 
 @Injectable()
 export class RoomsTypesAmenitiesService {
   constructor(
     private readonly roomsTypesAmenitiesRepository: RoomsTypesAmenitiesRepository,
   ) {}
+
+  private plainToInstanceDto(data: unknown): any {
+    return plainToInstance(RoomTypeAmenityResponseDto, data, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
