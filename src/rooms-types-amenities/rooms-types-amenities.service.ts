ed3afdae1f02979c614b1bb34914bd82cdb1ff20
JosueEC <josueev.cruz@gmail.com>
2025-01-08 18:54:13 -0600

    [FEAT](rooms-types-amenities.service): Inject the rooms-types-amenities-repository to the constructor class

--- src/rooms-types-amenities/rooms-types-amenities.service.ts
+++ src/rooms-types-amenities/rooms-types-amenities.service.ts
@@ -1,1 +1,10 @@
-export class RoomsTypesAmenitiesService {}
+import { Injectable } from '@nestjs/common';
+
+import { RoomsTypesAmenitiesRepository } from './repository/rooms-types-amenities.repository';
+
+@Injectable()
+export class RoomsTypesAmenitiesService {
+  constructor(
+    private readonly roomsTypesAmenitiesRepository: RoomsTypesAmenitiesRepository,
+  ) {}
+}
