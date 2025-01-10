3cf83f06f9301fe694044867e81919a061988db0
JosueEC <josueev.cruz@gmail.com>
2025-01-09 21:56:23 -0600

    [FEAT](rooms-types-amenities.controller): Inject the rooms-types-amenities-service to the class constructor

--- src/rooms-types-amenities/rooms-types-amenities.controller.ts
+++ src/rooms-types-amenities/rooms-types-amenities.controller.ts
@@ -1,1 +1,10 @@
-export class RoomsTypesAmenitiesController {}
+import { Controller } from '@nestjs/common';
+
+import { RoomsTypesAmenitiesService } from './rooms-types-amenities.service';
+
+@Controller()
+export class RoomsTypesAmenitiesController {
+  constructor(
+    private readonly roomsTypesAmenitiesService: RoomsTypesAmenitiesService,
+  ) {}
+}
