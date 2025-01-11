eca4a69938ee14f0a354cda8ff96c52ed9a84f78
JosueEC <josueev.cruz@gmail.com>
2025-01-10 19:45:57 -0600

    [REFACTOR](base-repository): Modify the update params by an fin-options-where

--- src/common/repository/base-repository.interface.ts
+++ src/common/repository/base-repository.interface.ts
@@ -1,13 +1,13 @@
-import { QueryRunner } from 'typeorm';
+import { FindOptionsWhere, QueryRunner } from 'typeorm';
 import { DeleteResultResponse } from '../dto/response';
 
-export interface IBaseRepository<T, CreateDto, UpdateDto> {
+export interface IBaseRepository<T, CreateDto> {
   setQueryRunner(queryRunner: QueryRunner): void;
 
   findAll(): Promise<T[]>;
   findOne(id: string): Promise<T>;
   create(request: Partial<T>): T;
   save(request: CreateDto): Promise<T>;
-  update(request: UpdateDto): Promise<T>;
+  update(conditions: FindOptionsWhere<T>, request: Partial<T>): Promise<T>;
   remove(id: string): Promise<DeleteResultResponse>;
 }
