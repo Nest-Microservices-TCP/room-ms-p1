8f13ec478e8a45a64e8c2e9198aea7f6eb676fb0
JosueEC <josueev.cruz@gmail.com>
2025-01-10 19:54:06 -0600

    [REFACTOR](extras..repository.interface): Remove the update-extra-dto from the interface params

--- src/extras/repository/interfaces/extras.repository.interface.ts
+++ src/extras/repository/interfaces/extras.repository.interface.ts
@@ -1,6 +1,5 @@
 import { IRepository } from 'src/common/repository';
-import { CreateExtraDto, UpdateExtraDto } from 'src/extras/dto/request';
+import { CreateExtraDto } from 'src/extras/dto/request';
 import { Extra } from 'src/extras/entity/extra.entity';
 
-export interface IExtrasRepository
-  extends IRepository<Extra, CreateExtraDto, UpdateExtraDto> {}
+export interface IExtrasRepository extends IRepository<Extra, CreateExtraDto> {}
