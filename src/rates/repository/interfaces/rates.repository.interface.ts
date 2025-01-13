624c543ef7c462dbf7b16e8360bf8711c9737315
JosueEC <josueev.cruz@gmail.com>
2025-01-12 20:10:58 -0600

    [REFACTOR](rates.repository.interface): Remove the update-rate-dto from the interface params

--- src/rates/repository/interfaces/rates.repository.interface.ts
+++ src/rates/repository/interfaces/rates.repository.interface.ts
@@ -1,6 +1,5 @@
 import { IRepository } from 'src/common/repository';
-import { CreateRateDto, UpdateRateDto } from 'src/rates/dto/request';
+import { CreateRateDto } from 'src/rates/dto/request';
 import { Rate } from 'src/rates/entity/rate.entity';
 
-export interface IRatesRepository
-  extends IRepository<Rate, CreateRateDto, UpdateRateDto> {}
+export interface IRatesRepository extends IRepository<Rate, CreateRateDto> {}
