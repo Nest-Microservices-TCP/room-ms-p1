babf67addeaf5c339d5efca53300f44e47257604
JosueEC <josueev.cruz@gmail.com>
2025-01-12 20:21:28 -0600

    [REFACTOR](rents.repository.interface): Remove the update dto from the interface params

--- src/rents/repository/interfaces/rents.repository.interface.ts
+++ src/rents/repository/interfaces/rents.repository.interface.ts
@@ -1,6 +1,5 @@
 import { IRepository } from 'src/common/repository';
-import { CreateRentDto, UpdateRentDto } from 'src/rents/dto/request';
+import { CreateRentDto } from 'src/rents/dto/request';
 import { Rent } from 'src/rents/entity';
 
-export interface IRentsRepository
-  extends IRepository<Rent, CreateRentDto, UpdateRentDto> {}
+export interface IRentsRepository extends IRepository<Rent, CreateRentDto> {}
