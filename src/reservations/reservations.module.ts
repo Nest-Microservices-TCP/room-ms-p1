2921c7c97ad30beef3dc6a7967af68aeb119692a
JosueEC <josueev.cruz@gmail.com>
2025-01-13 19:38:45 -0600

    [REFACTOR](reservations.module): Sort the imports sentences

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -1,9 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Reservation } from './entity/reservation.entity';
+
 import { ReservationsRepository } from './repository/reservations.repository';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
-import { Reservation } from './entity/reservation.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Reservation])],
