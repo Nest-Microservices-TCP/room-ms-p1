edd599034426fe9188705c16f6a4fd2967c20e97
JosueEC <josueev.cruz@gmail.com>
2025-01-13 19:35:42 -0600

    [REFACTOR](reservations.repository.interface): Remove the update dto from the interface params

--- src/reservations/repository/interfaces/reservations.repository.interface.ts
+++ src/reservations/repository/interfaces/reservations.repository.interface.ts
@@ -1,13 +1,6 @@
 import { IRepository } from 'src/common/repository';
-import {
-  CreateReservationDto,
-  UpdateReservationDto,
-} from 'src/reservations/dto/request';
+import { CreateReservationDto } from 'src/reservations/dto/request';
 import { Reservation } from 'src/reservations/entity/reservation.entity';
 
 export interface IReservationsRepository
-  extends IRepository<
-    Reservation,
-    CreateReservationDto,
-    UpdateReservationDto
-  > {}
+  extends IRepository<Reservation, CreateReservationDto> {}
