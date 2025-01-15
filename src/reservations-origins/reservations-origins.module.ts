0e3df221063140d49fe372dd0a9af74792b584ef
JosueEC <josueev.cruz@gmail.com>
2025-01-14 20:20:13 -0600

    [REFACTOR](reservations-origins.module): Sort and group the imports sentences

--- src/reservations-origins/reservations-origins.module.ts
+++ src/reservations-origins/reservations-origins.module.ts
@@ -1,9 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ReservationOrigin } from './entity/reservation-origin.entity';
+
 import { ReservationsOriginsRepository } from './repository/reservations-origins.repository';
 import { ReservationsOriginsController } from './reservations-origins.controller';
 import { ReservationsOriginsService } from './reservations-origins.service';
-import { ReservationOrigin } from './entity/reservation-origin.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ReservationOrigin])],
