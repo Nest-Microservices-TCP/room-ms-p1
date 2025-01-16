21cb58a4a18630368197e0b3163d8c832bfbaa7c
JosueEC <josueev.cruz@gmail.com>
2025-01-15 20:00:39 -0600

    [REFACTOR](rooms.repository.interface): Remove the update-dto from the interface params

--- src/rooms/repositories/interfaces/rooms.repository.interface.ts
+++ src/rooms/repositories/interfaces/rooms.repository.interface.ts
@@ -1,6 +1,5 @@
 import { IRepository } from 'src/common/repository';
-import { CreateRoomDto, UpdateRoomDto } from 'src/rooms/dto/request';
+import { CreateRoomDto } from 'src/rooms/dto/request';
 import { Room } from 'src/rooms/entity/room.entity';
 
-export interface IRoomsRepository
-  extends IRepository<Room, CreateRoomDto, UpdateRoomDto> {}
+export interface IRoomsRepository extends IRepository<Room, CreateRoomDto> {}
