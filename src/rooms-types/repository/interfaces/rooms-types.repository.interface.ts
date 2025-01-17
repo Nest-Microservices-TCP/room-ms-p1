29c8b2a1493d49abf3dc6df89e67462e7db1ae46
JosueEC <josueev.cruz@gmail.com>
2025-01-16 22:25:54 -0600

    [FEAT](rooms-types.repository): Remove the update dto from the interface params

--- src/rooms-types/repository/interfaces/rooms-types.repository.interface.ts
+++ src/rooms-types/repository/interfaces/rooms-types.repository.interface.ts
@@ -1,9 +1,6 @@
 import { IRepository } from 'src/common/repository';
-import {
-  CreateRoomTypeDto,
-  UpdateRoomTypeDto,
-} from 'src/rooms-types/dto/request';
+import { CreateRoomTypeDto } from 'src/rooms-types/dto/request';
 import { RoomType } from 'src/rooms-types/entity/room-type.entity';
 
 export interface IRoomsTypesRepository
-  extends IRepository<RoomType, CreateRoomTypeDto, UpdateRoomTypeDto> {}
+  extends IRepository<RoomType, CreateRoomTypeDto> {}
