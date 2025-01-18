91cf4cc8769680bd21ed3a1ce649146848f8b748
JosueEC <josueev.cruz@gmail.com>
2025-01-17 21:06:33 -0600

    [FEAT](rooms-types.module): Sort and group the imports sentences

--- src/rooms-types/rooms-types.module.ts
+++ src/rooms-types/rooms-types.module.ts
@@ -1,9 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { RoomType } from './entity/room-type.entity';
+
 import { RoomsTypesRepository } from './repository/rooms-types.repository';
 import { RoomsTypesController } from './rooms-types.controller';
 import { RoomsTypesService } from './rooms-types.service';
-import { RoomType } from './entity/room-type.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RoomType])],
