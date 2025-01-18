e1f10c6312ecbe70a7072970707dee934690d7a5
JosueEC <josueev.cruz@gmail.com>
2025-01-17 21:11:18 -0600

    [FEAT](rooms-types-amenities.module): Inject the typeorm-module feature config

--- src/rooms-types-amenities/rooms-types-amenities.module.ts
+++ src/rooms-types-amenities/rooms-types-amenities.module.ts
@@ -1,1 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { RoomTypeAmenity } from './entity/room-type-amenity.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([RoomTypeAmenity])],
+})
 export class RoomsTypesAmenitiesModule {}
