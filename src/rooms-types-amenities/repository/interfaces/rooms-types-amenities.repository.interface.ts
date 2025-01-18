2983d3c5cfd99007b59544ed4efe5a79c457623e
JosueEC <josueev.cruz@gmail.com>
2025-01-17 21:08:54 -0600

    [FEAT](rooms-types-amenities.repository.interface): Remove the update dto from the interface params

--- src/rooms-types-amenities/repository/interfaces/rooms-types-amenities.repository.interface.ts
+++ src/rooms-types-amenities/repository/interfaces/rooms-types-amenities.repository.interface.ts
@@ -1,15 +1,8 @@
 import { IBaseRepository } from 'src/common/repository';
 
-import {
-  CreateRoomTypeAmenityDto,
-  UpdateRoomTypeAmenityDto,
-} from 'src/rooms-types-amenities/dto/request';
+import { CreateRoomTypeAmenityDto } from 'src/rooms-types-amenities/dto/request';
 
 import { RoomTypeAmenity } from 'src/rooms-types-amenities/entity/room-type-amenity.entity';
 
 export interface IRoomsTypesAmenities
-  extends IBaseRepository<
-    RoomTypeAmenity,
-    CreateRoomTypeAmenityDto,
-    UpdateRoomTypeAmenityDto
-  > {}
+  extends IBaseRepository<RoomTypeAmenity, CreateRoomTypeAmenityDto> {}
