3cb140ef611bd70a4e50c9d70f20a3025447224f
JosueEC <josueev.cruz@gmail.com>
2025-01-23 18:07:29 -0600

    [REFACTOR](app.module): Sort and group the imports sentences

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
-import { ReservationsOriginsModule } from './reservations-origins/reservations-origins.module';
-import { ReservationsStatesModule } from './reservations-states/reservations-states.module';
+import { Module } from '@nestjs/common';
+
 import { PostgresConfigModule } from './database/postgres.config.module';
-import { ReservationsModule } from './reservations/reservations.module';
-import { RentsExtrasModule } from './rents-extras/rents-extras.module';
-import { RoomsStatesModule } from './rooms-states/rooms-states.module';
-import { RoomsTypesModule } from './rooms-types/rooms-types.module';
+
 import { AmenitiesModule } from './amenities/amenities.module';
 import { ExtrasModule } from './extras/extras.module';
-import { RoomsModule } from './rooms/rooms.module';
 import { RatesModule } from './rates/rates.module';
+import { RentsExtrasModule } from './rents-extras/rents-extras.module';
 import { RentsModule } from './rents/rents.module';
-import { Module } from '@nestjs/common';
+import { ReservationsOriginsModule } from './reservations-origins/reservations-origins.module';
+import { ReservationsStatesModule } from './reservations-states/reservations-states.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { RoomsStatesModule } from './rooms-states/rooms-states.module';
+import { RoomsTypesModule } from './rooms-types/rooms-types.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
   imports: [
