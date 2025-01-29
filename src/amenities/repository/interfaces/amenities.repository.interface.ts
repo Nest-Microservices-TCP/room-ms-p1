31d1630b27ac19a8a1cc9be5fcfc17dcdec502b3
JosueEC <josueev.cruz@gmail.com>
2025-01-28 19:41:31 -0600

    [REFACTOR](amenities.repository.interface): Remove the update dto from the interface params

--- src/amenities/repository/interfaces/amenities.repository.interface.ts
+++ src/amenities/repository/interfaces/amenities.repository.interface.ts
@@ -1,6 +1,6 @@
-import { CreateAmenityDto, UpdateAmenityDto } from 'src/amenities/dto/request';
+import { CreateAmenityDto } from 'src/amenities/dto/request';
 import { Amenity } from 'src/amenities/entity/amenity.entity';
 import { IRepository } from 'src/common/repository';
 
 export interface IAmenitiesRepository
-  extends IRepository<Amenity, CreateAmenityDto, UpdateAmenityDto> {}
+  extends IRepository<Amenity, CreateAmenityDto> {}
