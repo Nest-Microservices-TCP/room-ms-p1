405b1be86417b3412839d14dcbbfce9f0ea7e0c0
JosueEC <josueev.cruz@gmail.com>
2025-02-02 19:37:27 -0600

    [DB](rooms-states.seed.sql): Update the SQL instructions

--- src/database/seeds/rooms-states.seed.sql
+++ src/database/seeds/rooms-states.seed.sql
@@ -1,17 +1,14 @@
-TRUNCATE rooms_states ;
-
-INSERT INTO public.rooms_states
-(created_by, updated_by, status, deleted_at, "name")
-values
-	(NULL, NULL, 'active', NULL, 'Preparada'),
-	(NULL, NULL, 'active', NULL, 'Sucia'),
-	(NULL, NULL, 'active', NULL, 'A la venta'),
-	(NULL, NULL, 'active', NULL, 'Ocupada'),
-	(NULL, NULL, 'active', NULL, 'Supervisión'),
-	(NULL, NULL, 'active', NULL, 'Mantenimiento'),
-	(NULL, NULL, 'active', NULL, 'Limpieza'),
-	(NULL, NULL, 'active', NULL, 'Supervisión pendiente'),
-	(NULL, NULL, 'active', NULL, 'Supervisión en curso'),
-	(NULL, NULL, 'active', NULL, 'Reservada'),
-	(NULL, NULL, 'active', NULL, 'Bloqueada'),
-	(NULL, NULL, 'active', NULL, 'Room service');
+INSERT INTO public.rooms_states (status, "name")
+VALUES
+('Preparada'),
+('Sucia'),
+('A la venta'),
+('Ocupada'),
+('Supervisión'),
+('Mantenimiento'),
+('Limpieza'),
+('Supervisión pendiente'),
+('Supervisión en curso'),
+('Reservada'),
+('Bloqueada'),
+('Room service');
