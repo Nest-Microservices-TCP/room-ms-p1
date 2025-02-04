fe17af96429a3d0b41893192feca6c687f2789c9
JosueEC <josueev.cruz@gmail.com>
2025-02-03 20:29:29 -0600

    [DB](database/seeds): Add the reservations-origins-seed

---
+++ src/database/seeds/reservations-origins.sql
@@ -0,0 +1,17 @@
+INSERT INTO reservations_origins ("name")
+VALUES 
+('Teléfono'),
+('Recepción'),
+('Email'),
+('Expedia'),
+('Booking'),
+('Tripadvisor'),
+('Trivago'),
+('Despegar'),
+('Bestday'),
+('Checkfront'),
+('Priceline'),
+('Kayak'),
+('Hotels'),
+('Hotwire'),
+('Otro');
