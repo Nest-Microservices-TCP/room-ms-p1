ecfbef198499cd89435ae558db5c1fc53f40ded4
JosueEC <josueev.cruz@gmail.com>
2025-02-04 19:58:04 -0600

    [FEAT](rents.module): Add the rates-module to the imports dependencies array

--- src/rents/rents.module.ts
+++ src/rents/rents.module.ts
@@ -1,12 +1,16 @@
-import { RentsRepository } from './repository/rents.repository';
-import { RentsController } from './rents.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { RentsService } from './rents.service';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Rent } from './entity';
 
+import { RentsController } from './rents.controller';
+import { RentsService } from './rents.service';
+import { RentsRepository } from './repository/rents.repository';
+
+import { RatesModule } from 'src/rates/rates.module';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Rent])],
+  imports: [TypeOrmModule.forFeature([Rent]), RatesModule],
   controllers: [RentsController],
   providers: [RentsRepository, RentsService],
 })
