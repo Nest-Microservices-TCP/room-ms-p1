73bb5ac3c854f2fedad0b3090e2264ee461fa7c4
JosueEC <josueev.cruz@gmail.com>
2025-02-09 20:47:25 -0600

    [FEAT](rooms.controller): Expose the find.one.room for the rooms topic

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,8 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateRoomDto, UpdateRoomDto } from './dto/request';
 import { RoomResponseDto } from './dto/response';
 
 import { RoomsService } from './rooms.service';
@@ -11,33 +9,8 @@ import { RoomsService } from './rooms.service';
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
-  @MessagePattern({ cmd: 'save.room' })
-  save(@Payload() request: CreateRoomDto): Promise<RoomResponseDto> {
-    return this.roomsService.save(request);
-  }
-
-  @MessagePattern({ cmd: 'find.one.room' })
+  @MessagePattern('rooms.find.one.room')
   findOne(@Payload('roomId') roomId: string): Promise<RoomResponseDto> {
     return this.roomsService.findOne(roomId);
-  }
-
-  @MessagePattern({ cmd: 'find.rooms.by.ids' })
-  findByIds(@Payload() roomsIds: string[]): Promise<RoomResponseDto[]> {
-    return this.roomsService.findByIds(roomsIds);
-  }
-
-  @MessagePattern({ cmd: 'find.all.rooms' })
-  findAll(): Promise<RoomResponseDto[]> {
-    return this.roomsService.findAll();
-  }
-
-  @MessagePattern({ cmd: 'update.room' })
-  update(@Payload() request: UpdateRoomDto): Promise<RoomResponseDto> {
-    return this.roomsService.update(request);
-  }
-
-  @MessagePattern({ cmd: 'remove.room.by.id' })
-  remove(@Payload('roomId') roomId: string): Promise<DeleteResultResponse> {
-    return this.roomsService.remove(roomId);
   }
 }
