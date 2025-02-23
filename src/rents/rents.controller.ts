f9bfc8a5d273d30718a1b2fce2981ceb6ee9735a
JosueEC <josueev.cruz@gmail.com>
2025-02-22 21:55:53 -0600

    [REFACTOR](rents.controller): Replace the rooms topic by the rents topic

--- src/rents/rents.controller.ts
+++ src/rents/rents.controller.ts
@@ -10,32 +10,32 @@ import { RentsService } from './rents.service';
 export class RentsController {
   constructor(private readonly rentsService: RentsService) {}
 
-  @MessagePattern('rooms.find.all.rents')
+  @MessagePattern('rents.find.all')
   async findAll(): Promise<RentResponseDto[]> {
     return this.rentsService.findAll();
   }
 
-  @MessagePattern('rooms.find.one.rent')
+  @MessagePattern('rents.find.one')
   async findOne(@Payload('rentId') rentId: string): Promise<RentResponseDto> {
     return this.rentsService.findOne(rentId);
   }
 
-  @MessagePattern('rooms.find.rents.by.ids')
+  @MessagePattern('rents.find.by.ids')
   async findByIds(@Payload() rentsIds: string[]): Promise<RentResponseDto[]> {
     return this.rentsService.findByIds(rentsIds);
   }
 
-  @MessagePattern('rooms.save.rent')
+  @MessagePattern('rents.save')
   async save(@Payload() request: CreateRentDto): Promise<RentResponseDto> {
     return this.rentsService.save(request);
   }
 
-  @MessagePattern('rooms.update.rent')
+  @MessagePattern('rents.update')
   async update(@Payload() request: UpdateRentDto): Promise<RentResponseDto> {
     return this.rentsService.update(request);
   }
 
-  @MessagePattern('rooms.remove.rent')
+  @MessagePattern('rents.remove')
   async remove(@Payload('rentId') rentId: string): Promise<RentResponseDto> {
     return this.rentsService.remove(rentId);
   }
