08f4a58e016ee547f5a1a07c8481cf9d5eb311fb
JosueEC <josueev.cruz@gmail.com>
2025-02-25 22:31:23 -0600

    [REFACTOR](rates.service): Use the types of grpc files generated from rates proto files

--- src/rates/rates.service.ts
+++ src/rates/rates.service.ts
@@ -1,66 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
 
 import { RatesRepository } from './repository/rates.repository';
 
 import { HandleRpcExceptions } from 'src/common/decorators';
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateRateDto, UpdateRateDto } from './dto/request';
-import { RateResponseDto } from './dto/response';
+
+import { FindOneRateRequest } from 'src/grpc/rooms/rates/rates-request.pb';
+import { Rate } from 'src/grpc/rooms/rates/rates-response.pb';
 
 @Injectable()
 export class RatesService {
   constructor(private readonly ratesRepository: RatesRepository) {}
 
-  private plainToInstanceDto(data: unknown): any {
-    return plainToInstance(RateResponseDto, data, {
-      excludeExtraneousValues: true,
-    });
-  }
-
   @HandleRpcExceptions()
-  async findAll(): Promise<RateResponseDto[]> {
-    const rates = await this.ratesRepository.findAll();
+  async findOne(request: FindOneRateRequest): Promise<Rate> {
+    const { rate_id } = request;
 
-    return this.plainToInstanceDto(rates);
-  }
-
-  @HandleRpcExceptions()
-  async findOne(rateId: string): Promise<RateResponseDto> {
-    const rate = await this.ratesRepository.findOne(rateId);
-
-    return this.plainToInstanceDto(rate);
-  }
-
-  @HandleRpcExceptions()
-  async findByIds(ratesIds: string[]): Promise<RateResponseDto[]> {
-    const rates = await this.ratesRepository.findByIds(ratesIds);
-
-    return this.plainToInstanceDto(rates);
-  }
-
-  @HandleRpcExceptions()
-  async save(request: CreateRateDto): Promise<RateResponseDto> {
-    const newRate = await this.ratesRepository.save(request);
-
-    return this.plainToInstanceDto(newRate);
-  }
-
-  @HandleRpcExceptions()
-  async update(request: UpdateRateDto): Promise<RateResponseDto> {
-    const { rateId, ...rest } = request;
-
-    const updatedRate = await this.ratesRepository.update({ rateId }, rest);
-
-    return this.plainToInstanceDto(updatedRate);
-  }
-
-  @HandleRpcExceptions()
-  async remove(rateId: string): Promise<DeleteResultResponse> {
-    const deleteResult = await this.ratesRepository.remove(rateId);
-
-    return plainToInstance(DeleteResultResponse, deleteResult, {
-      excludeExtraneousValues: true,
-    });
+    return await this.ratesRepository.findOne(rate_id);
   }
 }
