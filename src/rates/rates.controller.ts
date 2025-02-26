89af39558eddea614c16015d0cb3e35464031e2b
JosueEC <josueev.cruz@gmail.com>
2025-02-25 22:30:53 -0600

    [REFACTOR](rates.controller): Use the types of grpc files generated from rates proto files

--- src/rates/rates.controller.ts
+++ src/rates/rates.controller.ts
@@ -1,46 +1,23 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { plainToInstance } from 'class-transformer';
+import { Observable } from 'rxjs';
 
 import { RatesService } from './rates.service';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateRateDto, UpdateRateDto } from './dto/request';
-import { RateResponseDto } from './dto/response';
+import { FindOneRateRequest } from 'src/grpc/rooms/rates/rates-request.pb';
+import { Rate } from 'src/grpc/rooms/rates/rates-response.pb';
+import {
+  RatesServiceController,
+  RatesServiceControllerMethods,
+} from 'src/grpc/rooms/rates/rates.pb';
 
 @Controller()
-export class RatesController {
+@RatesServiceControllerMethods()
+export class RatesController implements RatesServiceController {
   constructor(private readonly ratesService: RatesService) {}
 
-  @MessagePattern('rates.find.all')
-  async findAll(): Promise<RateResponseDto[]> {
-    return this.ratesService.findAll();
-  }
-
-  @MessagePattern('rates.find.one')
-  async findOne(@Payload('rateId') rateId: string): Promise<any> {
-    //TODO: Se esta serializando mal el dto, hay que revisar que esta fallando
-    const rate = await this.ratesService.findOne(rateId);
-
-    return JSON.stringify(
-      plainToInstance(RateResponseDto, rate, { excludeExtraneousValues: true }),
-    );
-  }
-
-  @MessagePattern('rates.save')
-  async save(@Payload() request: CreateRateDto): Promise<RateResponseDto> {
-    return this.ratesService.save(request);
-  }
-
-  @MessagePattern('rates.update')
-  async update(@Payload() request: UpdateRateDto): Promise<RateResponseDto> {
-    return this.ratesService.update(request);
-  }
-
-  @MessagePattern('rates.remove')
-  async remove(
-    @Payload('rateId') rateId: string,
-  ): Promise<DeleteResultResponse> {
-    return this.ratesService.remove(rateId);
+  findOne(
+    request: FindOneRateRequest,
+  ): Promise<Rate> | Observable<Rate> | Rate {
+    return this.ratesService.findOne(request);
   }
 }
