e62186677662a067f1577c14a2a0c698af0edb63
JosueEC <josueev.cruz@gmail.com>
2025-02-27 22:38:11 -0600

    [REFACTOR](rooms/dto/response): Delete the directory

--- src/rooms/dto/response/room.response.dto.ts
+++
@@ -1,7 +0,0 @@
-import { BaseResponseDto } from 'src/common/dto/response';
-import { Expose } from 'class-transformer';
-
-export class RoomResponseDto extends BaseResponseDto {
-  @Expose()
-  name: string;
-}
