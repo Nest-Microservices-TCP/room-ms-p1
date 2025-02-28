7b39ae1a9f65c41393d8dd35f58f51d24ebf0d05
JosueEC <josueev.cruz@gmail.com>
2025-02-27 22:36:12 -0600

    [REFACTOR](amenities/dto): Delete the directory

--- src/amenities/dto/request/update-amenity.dto.ts
+++
@@ -1,8 +0,0 @@
-import { CreateAmenityDto } from './create-amenity.dto';
-import { PartialType } from '@nestjs/mapped-types';
-import { IsUUID } from 'class-validator';
-
-export class UpdateAmenityDto extends PartialType(CreateAmenityDto) {
-  @IsUUID()
-  amenityId: string;
-}
