c82c29ea6a52a32c607bc8c2f85c6dad2b7b7403
JosueEC <josueev.cruz@gmail.com>
2025-02-27 22:40:11 -0600

    [FEAT](amenity.entity): Implement the Amenity interface from proto types

--- src/amenities/entity/amenity.entity.ts
+++ src/amenities/entity/amenity.entity.ts
@@ -1,16 +1,16 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Amenity as IAmenity } from 'src/grpc/rooms/amenities.pb';
 
 import { BaseEntity } from 'src/common/entity';
-
 import { RoomTypeAmenity } from 'src/rooms-types-amenities/entity/room-type-amenity.entity';
 
 @Entity({ name: 'amenities' })
-export class Amenity extends BaseEntity {
+export class Amenity extends BaseEntity implements IAmenity {
   @PrimaryGeneratedColumn('uuid', {
     primaryKeyConstraintName: 'PK_Amenities',
     name: 'amenity_id',
   })
-  amenityId: string;
+  amenity_id: string;
 
   @Column({
     name: 'name',
