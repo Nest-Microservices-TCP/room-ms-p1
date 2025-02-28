182f894215110769e871095d8236e9879e1a1021
JosueEC <josueev.cruz@gmail.com>
2025-02-27 22:39:36 -0600

    [REFACTOR](amenities.service): Use the proto types for the service methods

--- src/amenities/amenities.service.ts
+++ src/amenities/amenities.service.ts
@@ -1,70 +1,37 @@
 import { Injectable } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
 
 import { HandleRpcExceptions } from 'src/common/decorators';
 
+import {
+  GetAmenityRequest,
+  CreateAmenityRequest,
+  ListAmenitiesResponse,
+} from 'src/grpc/rooms/amenities.pb';
+
 import { AmenitiesRepository } from './repository/amenities.repository';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateAmenityDto, UpdateAmenityDto } from './dto/request';
-import { AmenityResponseDto } from './dto/response';
+import { Amenity } from './entity/amenity.entity';
 
 @Injectable()
 export class AmenitiesService {
   constructor(private readonly amenitiesRepository: AmenitiesRepository) {}
 
-  private plainToInstanceDto(data: unknown): any {
-    return plainToInstance(AmenityResponseDto, data, {
-      excludeExtraneousValues: true,
-    });
+  @HandleRpcExceptions()
+  createAmenity(request: CreateAmenityRequest): void {
+    this.amenitiesRepository.save(request);
   }
 
   @HandleRpcExceptions()
-  async findAll(): Promise<AmenityResponseDto[]> {
+  async listAmenities(): Promise<ListAmenitiesResponse> {
     const amenities = await this.amenitiesRepository.findAll();
 
-    return this.plainToInstanceDto(amenities);
+    return { amenities };
   }
 
   @HandleRpcExceptions()
-  async findOne(amenityId: string): Promise<AmenityResponseDto> {
-    const amenity = await this.amenitiesRepository.findOne(amenityId);
+  async getAmenity(request: GetAmenityRequest): Promise<Amenity> {
+    const { amenity_id } = request;
 
-    return this.plainToInstanceDto(amenity);
-  }
-
-  @HandleRpcExceptions()
-  async findByIds(amenitiesIds: string[]): Promise<AmenityResponseDto[]> {
-    const amenities = await this.amenitiesRepository.findByIds(amenitiesIds);
-
-    return this.plainToInstanceDto(amenities);
-  }
-
-  @HandleRpcExceptions()
-  async save(request: CreateAmenityDto): Promise<AmenityResponseDto> {
-    const newAmenity = await this.amenitiesRepository.save(request);
-
-    return this.plainToInstanceDto(newAmenity);
-  }
-
-  @HandleRpcExceptions()
-  async update(request: UpdateAmenityDto): Promise<AmenityResponseDto> {
-    const { amenityId, ...rest } = request;
-
-    const updatedAmenity = await this.amenitiesRepository.update(
-      { amenityId },
-      rest,
-    );
-
-    return this.plainToInstanceDto(updatedAmenity);
-  }
-
-  @HandleRpcExceptions()
-  async remove(amenityId: string): Promise<DeleteResultResponse> {
-    const deleteResult = await this.amenitiesRepository.remove(amenityId);
-
-    return plainToInstance(DeleteResultResponse, deleteResult, {
-      excludeExtraneousValues: true,
-    });
+    return this.amenitiesRepository.findOne(amenity_id);
   }
 }
