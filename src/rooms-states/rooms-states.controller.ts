2c7c47442c64d1149294b0c929d2f9c8425a704d
JosueEC <josueev.cruz@gmail.com>
2025-03-01 21:07:57 -0600

    [REFACTOR](rooms-states.controller): Use the types provided by the proto files compilation

--- src/rooms-states/rooms-states.controller.ts
+++ src/rooms-states/rooms-states.controller.ts
@@ -1,57 +1,27 @@
+import { Observable } from 'rxjs';
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
 import {
-  CreateRoomStateDto,
-  FindOneRoomStateByIdDto,
-  UpdateRoomStateDto,
-} from './dto/request';
-import { RoomStateResponseDto } from './dto/response';
+  RoomState,
+  GetRoomStateRequest,
+  CreateRoomStateRequest,
+  RoomsStatesServiceController,
+  RoomsStatesServiceControllerMethods,
+} from 'src/grpc/proto/rooms/rooms_states.pb';
 
 import { RoomsStatesService } from './rooms-states.service';
 
 @Controller()
-export class RoomsStatesController {
+@RoomsStatesServiceControllerMethods()
+export class RoomsStatesController implements RoomsStatesServiceController {
   constructor(private readonly roomsStatesService: RoomsStatesService) {}
 
-  @MessagePattern('roomsStates.save')
-  async save(
-    @Payload() request: CreateRoomStateDto,
-  ): Promise<RoomStateResponseDto> {
-    return this.roomsStatesService.save(request);
+  createRoomState(request: CreateRoomStateRequest): void {
+    this.roomsStatesService.save(request);
   }
-
-  @MessagePattern('roomsStates.find.one')
-  async findOne(
-    @Payload() request: FindOneRoomStateByIdDto,
-  ): Promise<RoomStateResponseDto> {
+  getRoomState(
+    request: GetRoomStateRequest,
+  ): Promise<RoomState> | Observable<RoomState> | RoomState {
     return this.roomsStatesService.findOne(request);
-  }
-
-  @MessagePattern('roomsStates.find.by.ids')
-  async findByIds(
-    @Payload('roomsStatesIds') roomsStatesIds: string[],
-  ): Promise<RoomStateResponseDto[]> {
-    return this.roomsStatesService.findByIds(roomsStatesIds);
-  }
-
-  @MessagePattern('roomsStates.find.all')
-  async findAll(): Promise<RoomStateResponseDto[]> {
-    return this.roomsStatesService.findAll();
-  }
-
-  @MessagePattern('roomsStates.update')
-  async update(
-    @Payload() request: UpdateRoomStateDto,
-  ): Promise<RoomStateResponseDto> {
-    return this.roomsStatesService.update(request);
-  }
-
-  @MessagePattern('roomsStates.remove')
-  async remove(
-    @Payload('roomStateId') roomStateId: string,
-  ): Promise<DeleteResultResponse> {
-    return this.roomsStatesService.remove(roomStateId);
   }
 }
