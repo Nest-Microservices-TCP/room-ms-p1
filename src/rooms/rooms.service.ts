6295d2c6580435389c75f6c8cad3d77b4545cfcd
JosueEC <josueev.cruz@gmail.com>
2025-03-05 21:47:11 -0600

    [REFACTOR](rooms.service): Rename the service methods and replace the request messages

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -3,33 +3,33 @@ import { Injectable } from '@nestjs/common';
 import { HandleRpcExceptions } from 'src/common/decorators';
 
 import {
-  GetRoomRequest,
-  ListRoomsResponse,
+  FindRoomsResponse,
   CreateRoomRequest,
+  FindOneRoomRequest,
 } from 'src/grpc/proto/rooms/rooms.pb';
 
-import { Room } from './entity/room.entity';
-
 import { RoomsRepository } from './repositories/rooms.repository';
+
+import { Room } from './entity/room.entity';
 
 @Injectable()
 export class RoomsService {
   constructor(private readonly roomsRepository: RoomsRepository) {}
 
   @HandleRpcExceptions()
-  createRoom(request: CreateRoomRequest): void {
+  save(request: CreateRoomRequest): void {
     this.roomsRepository.save(request);
   }
 
   @HandleRpcExceptions()
-  async getRoom(request: GetRoomRequest): Promise<Room> {
+  async findOne(request: FindOneRoomRequest): Promise<Room> {
     const { room_id } = request;
 
     return this.roomsRepository.findOne(room_id);
   }
 
   @HandleRpcExceptions()
-  async listRooms(): Promise<ListRoomsResponse> {
+  async find(): Promise<FindRoomsResponse> {
     const rooms = await this.roomsRepository.findAll();
 
     return { rooms };
