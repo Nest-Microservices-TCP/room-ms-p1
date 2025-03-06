90aa6b3cf2843efc548a92f118621fedd8831b5e
JosueEC <josueev.cruz@gmail.com>
2025-03-05 21:41:03 -0600

    [REFACTOR](rates.service): Rename the service methods

--- src/rates/rates.service.ts
+++ src/rates/rates.service.ts
@@ -4,9 +4,9 @@ import { HandleRpcExceptions } from 'src/common/decorators';
 import { RatesRepository } from './repository/rates.repository';
 
 import {
-  GetRateRequest,
-  ListRatesResponse,
+  FindRatesResponse,
   CreateRateRequest,
+  FindOneRateRequest,
 } from 'src/grpc/proto/rooms/rates.pb';
 
 import { Rate } from './entity/rate.entity';
@@ -16,19 +16,19 @@ export class RatesService {
   constructor(private readonly ratesRepository: RatesRepository) {}
 
   @HandleRpcExceptions()
-  createRate(request: CreateRateRequest): void {
+  save(request: CreateRateRequest): void {
     this.ratesRepository.save(request);
   }
 
   @HandleRpcExceptions()
-  async getRate(request: GetRateRequest): Promise<Rate> {
+  async findOne(request: FindOneRateRequest): Promise<Rate> {
     const { rate_id } = request;
 
     return await this.ratesRepository.findOne(rate_id);
   }
 
   @HandleRpcExceptions()
-  async listRates(): Promise<ListRatesResponse> {
+  async find(): Promise<FindRatesResponse> {
     const rates = await this.ratesRepository.findAll();
 
     return { rates };
