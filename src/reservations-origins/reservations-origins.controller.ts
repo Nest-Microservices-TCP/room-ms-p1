59dc2925d222c6196f2682c19ee20233dec6a281
JosueEC <josueev.cruz@gmail.com>
2025-03-06 20:56:51 -0600

    [REFACTOR](reservations-origins.controller): Use the types generated by the proto files

--- src/reservations-origins/reservations-origins.controller.ts
+++ src/reservations-origins/reservations-origins.controller.ts
@@ -1,56 +1,23 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
 import {
-  CreateReservationOriginDto,
-  UpdateReservationOriginDto,
-} from './dto/request';
-import { ReservationOriginResponseDto } from './dto/response';
+  CreateReservationOriginRequest,
+  ReservationsOriginsServiceController,
+  ReservationsOriginsServiceControllerMethods,
+} from 'src/grpc/proto/rooms/reservations_origins.pb';
 
 import { ReservationsOriginsService } from './reservations-origins.service';
 
 @Controller()
-export class ReservationsOriginsController {
+@ReservationsOriginsServiceControllerMethods()
+export class ReservationsOriginsController
+  implements ReservationsOriginsServiceController
+{
   constructor(
     private readonly reservationsOriginsService: ReservationsOriginsService,
   ) {}
 
-  @MessagePattern('reservationsOrigins.find.all')
-  async findAll(): Promise<ReservationOriginResponseDto[]> {
-    return this.reservationsOriginsService.findAll();
-  }
-
-  @MessagePattern('reservationsOrigins.find.one')
-  async findOne(
-    @Payload('reservationOriginId') reservationOriginId: string,
-  ): Promise<ReservationOriginResponseDto> {
-    return this.reservationsOriginsService.findOne(reservationOriginId);
-  }
-
-  @MessagePattern('reservationsOrigins.find.by.ids')
-  async findByIds(
-    @Payload('reservationsOriginsIds') reservationsOriginsIds: string[],
-  ): Promise<ReservationOriginResponseDto[]> {
-    return this.reservationsOriginsService.findByIds(reservationsOriginsIds);
-  }
-
-  @MessagePattern('reservationsOrigins.save')
-  async save(
-    @Payload() request: CreateReservationOriginDto,
-  ): Promise<ReservationOriginResponseDto> {
-    return this.reservationsOriginsService.save(request);
-  }
-
-  @MessagePattern('reservationsOrigins.update')
-  async update(
-    request: UpdateReservationOriginDto,
-  ): Promise<ReservationOriginResponseDto> {
-    return this.reservationsOriginsService.update(request);
-  }
-
-  @MessagePattern('reservationsOrigins.remove')
-  async remove(reservationOriginId: string): Promise<DeleteResultResponse> {
-    return this.reservationsOriginsService.remove(reservationOriginId);
+  save(request: CreateReservationOriginRequest): void {
+    this.reservationsOriginsService.save(request);
   }
 }
