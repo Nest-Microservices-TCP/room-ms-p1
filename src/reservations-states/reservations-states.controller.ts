0224cac20158339ee308b08d15105e3e6c6cb8cc
JosueEC <josueev.cruz@gmail.com>
2025-03-07 19:56:53 -0600

    [REFACTOR](reservations-stats.controller): Use the types generated by the proto files

--- src/reservations-states/reservations-states.controller.ts
+++ src/reservations-states/reservations-states.controller.ts
@@ -1,58 +1,23 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
 import {
-  CreateReservationStateDto,
-  UpdateReservationStateDto,
-} from './dto/request';
-import { ReservationStateResponseDto } from './dto/response';
+  CreateReservationStateRequest,
+  ReservationsStatesServiceController,
+  ReservationsStatesServiceControllerMethods,
+} from 'src/grpc/proto/rooms/reservations_states.pb';
 
 import { ReservationsStatesService } from './reservations-states.service';
 
 @Controller()
-export class ReservationsStatesController {
+@ReservationsStatesServiceControllerMethods()
+export class ReservationsStatesController
+  implements ReservationsStatesServiceController
+{
   constructor(
     private readonly reservationsStatesService: ReservationsStatesService,
   ) {}
 
-  @MessagePattern('reservationsStates.find.all')
-  async findAll(): Promise<ReservationStateResponseDto[]> {
-    return this.reservationsStatesService.findAll();
-  }
-
-  @MessagePattern('reservationsStates.find.one')
-  async findOne(
-    @Payload('reservationStateId') reservationStateId: string,
-  ): Promise<ReservationStateResponseDto> {
-    return this.reservationsStatesService.findOne(reservationStateId);
-  }
-
-  @MessagePattern('reservationsStates.find.by.ids')
-  async findByIds(
-    reservationsStatesIds: string[],
-  ): Promise<ReservationStateResponseDto[]> {
-    return this.reservationsStatesService.findByIds(reservationsStatesIds);
-  }
-
-  @MessagePattern('reservationsStates.save')
-  async save(
-    @Payload() request: CreateReservationStateDto,
-  ): Promise<ReservationStateResponseDto> {
-    return this.reservationsStatesService.save(request);
-  }
-
-  @MessagePattern('reservationsStates.update')
-  async update(
-    @Payload() request: UpdateReservationStateDto,
-  ): Promise<ReservationStateResponseDto> {
-    return this.reservationsStatesService.update(request);
-  }
-
-  @MessagePattern('reservationsStates.remove')
-  async remove(
-    @Payload('reservationStateId') reservationStateId: string,
-  ): Promise<DeleteResultResponse> {
-    return this.reservationsStatesService.remove(reservationStateId);
+  save(request: CreateReservationStateRequest): void {
+    this.reservationsStatesService.save(request);
   }
 }
