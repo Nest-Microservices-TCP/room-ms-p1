fbc61d693b89052c3e023592acef4ae2816f8b91
JosueEC <josueev.cruz@gmail.com>
2025-03-07 19:57:04 -0600

    [REFACTOR](reservations-stats.service): Use the types generated by the proto files

--- src/reservations-states/reservations-states.service.ts
+++ src/reservations-states/reservations-states.service.ts
@@ -1,14 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
-
 import { HandleRpcExceptions } from 'src/common/decorators';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import {
-  CreateReservationStateDto,
-  UpdateReservationStateDto,
-} from './dto/request';
-import { ReservationStateResponseDto } from './dto/response';
+import { CreateReservationStateRequest } from 'src/grpc/proto/rooms/reservations_states.pb';
 
 import { ReservationsStatesRepository } from './repository/reservations-states.repository';
 
@@ -18,71 +11,8 @@ export class ReservationsStatesService {
     private readonly reservationsStatesRepository: ReservationsStatesRepository,
   ) {}
 
-  private plainToInstanceDto(data: unknown): any {
-    return plainToInstance(ReservationStateResponseDto, data, {
-      excludeExtraneousValues: true,
-    });
-  }
-
   @HandleRpcExceptions()
-  async findAll(): Promise<ReservationStateResponseDto[]> {
-    const reservations = await this.reservationsStatesRepository.findAll();
-
-    return this.plainToInstanceDto(reservations);
-  }
-
-  @HandleRpcExceptions()
-  async findOne(
-    reservationStateId: string,
-  ): Promise<ReservationStateResponseDto> {
-    const reservationState =
-      await this.reservationsStatesRepository.findOne(reservationStateId);
-
-    return this.plainToInstanceDto(reservationState);
-  }
-
-  @HandleRpcExceptions()
-  async findByIds(
-    reservationsStatesIds: string[],
-  ): Promise<ReservationStateResponseDto[]> {
-    const reservationsStates =
-      await this.reservationsStatesRepository.findByIds(reservationsStatesIds);
-
-    return this.plainToInstanceDto(reservationsStates);
-  }
-
-  @HandleRpcExceptions()
-  async save(
-    request: CreateReservationStateDto,
-  ): Promise<ReservationStateResponseDto> {
-    const newReservationState =
-      await this.reservationsStatesRepository.save(request);
-
-    return this.plainToInstanceDto(newReservationState);
-  }
-
-  @HandleRpcExceptions()
-  async update(
-    request: UpdateReservationStateDto,
-  ): Promise<ReservationStateResponseDto> {
-    const { reservationStateId, ...rest } = request;
-
-    const updatedReservationState =
-      await this.reservationsStatesRepository.update(
-        { reservationStateId },
-        rest,
-      );
-
-    return this.plainToInstanceDto(updatedReservationState);
-  }
-
-  @HandleRpcExceptions()
-  async remove(reservationStateId: string): Promise<DeleteResultResponse> {
-    const deleteResult =
-      await this.reservationsStatesRepository.remove(reservationStateId);
-
-    return plainToInstance(DeleteResultResponse, deleteResult, {
-      excludeExtraneousValues: true,
-    });
+  save(request: CreateReservationStateRequest): void {
+    this.reservationsStatesRepository.save(request);
   }
 }
