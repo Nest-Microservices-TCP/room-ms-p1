899efe882723aefbc138fdc12ef0c89b9ad9d383
JosueEC <josueev.cruz@gmail.com>
2025-03-07 20:25:35 -0600

    [REFACTOR](rooms-types.service): Use the types generated by the proto files

--- src/rooms-types/rooms-types.service.ts
+++ src/rooms-types/rooms-types.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
-
 import { HandleRpcExceptions } from 'src/common/decorators';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateRoomTypeDto, UpdateRoomTypeDto } from './dto/request';
-import { RoomTypeResponseDto } from './dto/response';
+import { CreateRoomTypeRequest } from 'src/grpc/proto/rooms/rooms_types.pb';
 
 import { RoomsTypesRepository } from './repository/rooms-types.repository';
 
@@ -13,58 +9,8 @@ import { RoomsTypesRepository } from './repository/rooms-types.repository';
 export class RoomsTypesService {
   constructor(private readonly roomsTypesRepository: RoomsTypesRepository) {}
 
-  private plainToInstanceDto(data: unknown): any {
-    return plainToInstance(RoomTypeResponseDto, data, {
-      excludeExtraneousValues: true,
-    });
-  }
-
   @HandleRpcExceptions()
-  async findAll(): Promise<RoomTypeResponseDto[]> {
-    const roomsTypes = await this.roomsTypesRepository.findAll();
-
-    return this.plainToInstanceDto(roomsTypes);
-  }
-
-  @HandleRpcExceptions()
-  async findOne(roomTypeId: string): Promise<RoomTypeResponseDto> {
-    const roomType = await this.roomsTypesRepository.findOne(roomTypeId);
-
-    return this.plainToInstanceDto(roomType);
-  }
-
-  @HandleRpcExceptions()
-  async findByIds(roomsTypesIds: string[]): Promise<RoomTypeResponseDto[]> {
-    const roomsTypes = await this.roomsTypesRepository.findByIds(roomsTypesIds);
-
-    return this.plainToInstanceDto(roomsTypes);
-  }
-
-  @HandleRpcExceptions()
-  async save(request: CreateRoomTypeDto): Promise<RoomTypeResponseDto> {
-    const newRoomType = await this.roomsTypesRepository.save(request);
-
-    return this.plainToInstanceDto(newRoomType);
-  }
-
-  @HandleRpcExceptions()
-  async update(request: UpdateRoomTypeDto): Promise<RoomTypeResponseDto> {
-    const { roomTypeId, ...rest } = request;
-
-    const roomTypeUpdated = await this.roomsTypesRepository.update(
-      { roomTypeId },
-      rest,
-    );
-
-    return this.plainToInstanceDto(roomTypeUpdated);
-  }
-
-  @HandleRpcExceptions()
-  async remove(roomTypeId: string): Promise<DeleteResultResponse> {
-    const deleteResult = await this.roomsTypesRepository.remove(roomTypeId);
-
-    return plainToInstance(DeleteResultResponse, deleteResult, {
-      excludeExtraneousValues: true,
-    });
+  save(request: CreateRoomTypeRequest): void {
+    this.roomsTypesRepository.save(request);
   }
 }
