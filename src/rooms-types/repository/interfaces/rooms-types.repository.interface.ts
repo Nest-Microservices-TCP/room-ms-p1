8c5bde0fbbcfc103fc45fa71a06a4a4901f26526
JosueEC <josueev.cruz@gmail.com>
2025-03-08 20:55:14 -0600

    [REFACTOR](rooms-types.repository.interface): Use the types generated by the proto files

--- src/rooms-types/repository/interfaces/rooms-types.repository.interface.ts
+++ src/rooms-types/repository/interfaces/rooms-types.repository.interface.ts
@@ -1,6 +1,6 @@
 import { IRepository } from 'src/common/repository';
-import { CreateRoomTypeDto } from 'src/rooms-types/dto/request';
+import { CreateRoomTypeRequest } from 'src/grpc/proto/rooms/rooms_types.pb';
 import { RoomType } from 'src/rooms-types/entity/room-type.entity';
 
 export interface IRoomsTypesRepository
-  extends IRepository<RoomType, CreateRoomTypeDto> {}
+  extends IRepository<RoomType, CreateRoomTypeRequest> {}
