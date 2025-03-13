5fab8f4289764f6909cf7d053ff94d95c6476761
JosueEC <josueev.cruz@gmail.com>
2025-03-12 22:00:05 -0600

    [DATABASE](seeds): Add the rooms-types seed script

---
+++ src/database/seeds/rooms-types.seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO rooms_types ("name", included_people)
+VALUES
+('Pool Villa', 2),
+('Handicap', 2),
+('Pool & Spa Suite', 2),
+('Junior Suite', 2),
+('Sky Suite', 2),
+('Master Suite', 2),
+('Junior Villa', 2);
