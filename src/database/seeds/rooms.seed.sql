8604b2ca115ed06823f4b05cf0d6a526b52e4a8f
JosueEC <josueev.cruz@gmail.com>
2025-03-12 22:00:31 -0600

    [DATABASE](seeds): Add the rooms seed script

---
+++ src/database/seeds/rooms.seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO rooms ("number" , room_state_id , room_type_id) 
+VALUES
+(101, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', 'df9a3b45-decc-4251-9a8d-fa792d2a0e46'),
+(102, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', 'df9a3b45-decc-4251-9a8d-fa792d2a0e46'),
+(103, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', 'df9a3b45-decc-4251-9a8d-fa792d2a0e46'),
+(104, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', 'df9a3b45-decc-4251-9a8d-fa792d2a0e46'),
+(105, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', 'b3c78fba-b7cc-44f5-9b0c-1e059ea9006f'),
+(106, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', 'b3c78fba-b7cc-44f5-9b0c-1e059ea9006f'),
+(107, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '9ecbe479-d1c9-4d56-9207-33436da79c5c'),
+(108, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '9ecbe479-d1c9-4d56-9207-33436da79c5c'),
+(109, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '38369700-e22f-4dde-ac2c-a86d808ceaef'),
+(110, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '38369700-e22f-4dde-ac2c-a86d808ceaef'),
+(111, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '38369700-e22f-4dde-ac2c-a86d808ceaef'),
+(112, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '38369700-e22f-4dde-ac2c-a86d808ceaef'),
+(113, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '38369700-e22f-4dde-ac2c-a86d808ceaef'),
+(114, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '934d14e9-b78b-49e6-a45f-ddb05ce3fa78'),
+(115, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '934d14e9-b78b-49e6-a45f-ddb05ce3fa78'),
+(116, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '934d14e9-b78b-49e6-a45f-ddb05ce3fa78'),
+(117, '1dd1c3d2-19e2-4c70-b61c-b7a5ea598aaa', '934d14e9-b78b-49e6-a45f-ddb05ce3fa78');
