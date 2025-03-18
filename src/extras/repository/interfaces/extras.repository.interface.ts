a2795921ef796e5f9312c4e3e55c01d4df4895b4
JosueEC <josueev.cruz@gmail.com>
2025-03-17 19:34:30 -0600

    [REFACTOR](extras.repository): Replace the repository interface by the base-repository interface implementation

--- src/extras/repository/interfaces/extras.repository.interface.ts
+++ src/extras/repository/interfaces/extras.repository.interface.ts
@@ -1,6 +1,6 @@
-import { IRepository } from 'src/common/repository';
+import { IBaseRepository } from 'src/common/repository';
 import { CreateExtraRequest } from 'src/grpc/proto/rooms/extras.pb';
 import { Extra } from 'src/extras/entity/extra.entity';
 
 export interface IExtrasRepository
-  extends IRepository<Extra, CreateExtraRequest> {}
+  extends IBaseRepository<Extra, CreateExtraRequest> {}
