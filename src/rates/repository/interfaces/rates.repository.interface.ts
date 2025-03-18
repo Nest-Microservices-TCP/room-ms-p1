d20ddfe18a13509ea0d59c94d34c145f4fb2a919
JosueEC <josueev.cruz@gmail.com>
2025-03-17 19:34:54 -0600

    [REFACTOR](rates.repository.interface): Replace the repository interface by the base-repository interface implementation

--- src/rates/repository/interfaces/rates.repository.interface.ts
+++ src/rates/repository/interfaces/rates.repository.interface.ts
@@ -1,6 +1,6 @@
-import { IRepository } from 'src/common/repository';
+import { IBaseRepository } from 'src/common/repository';
 import { CreateRateRequest } from 'src/grpc/proto/rooms/rates.pb';
 import { Rate } from 'src/rates/entity/rate.entity';
 
 export interface IRatesRepository
-  extends IRepository<Rate, CreateRateRequest> {}
+  extends IBaseRepository<Rate, CreateRateRequest> {}
