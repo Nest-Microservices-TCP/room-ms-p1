d6b64840875259a000e967d96075cde9dacc70e4
JosueEC <josueev.cruz@gmail.com>
2025-03-17 19:34:20 -0600

    [REFACTOR](amenities.repository): Replace the repository interface by the base-repository interface implementation

--- src/amenities/repository/interfaces/amenities.repository.interface.ts
+++ src/amenities/repository/interfaces/amenities.repository.interface.ts
@@ -1,6 +1,6 @@
-import { IRepository } from 'src/common/repository';
+import { IBaseRepository } from 'src/common/repository';
 import { CreateAmenityRequest } from 'src/grpc/proto/rooms/amenities.pb';
 import { Amenity } from 'src/amenities/entity/amenity.entity';
 
 export interface IAmenitiesRepository
-  extends IRepository<Amenity, CreateAmenityRequest> {}
+  extends IBaseRepository<Amenity, CreateAmenityRequest> {}
