48f8cae76023e0dd9d977967749573ad2c0552bc
JosueEC <josueev.cruz@gmail.com>
2025-03-17 19:35:22 -0600

    [REFACTOR](rents-extras.repository.interface): Replace the repository interface by the base-repository interface implementation

--- src/rents/repository/interfaces/rents.repository.interface.ts
+++ src/rents/repository/interfaces/rents.repository.interface.ts
@@ -1,6 +1,7 @@
-import { IRepository } from 'src/common/repository';
+import { IBaseRepository } from 'src/common/repository';
 import { Rent } from 'src/rents/entity';
 
 export class CreateRentDto {}
 
-export interface IRentsRepository extends IRepository<Rent, CreateRentDto> {}
+export interface IRentsRepository
+  extends IBaseRepository<Rent, CreateRentDto> {}
