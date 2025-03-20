ffd257adb8fba5beb81ec20fdd9a1532ac8261a5
JosueEC <josueev.cruz@gmail.com>
2025-03-19 18:50:27 -0600

    [REFACTOR](rents.controller): Use just the save controller endpoint and remove the rest of endpoints

--- src/rents/rents.controller.ts
+++ src/rents/rents.controller.ts
@@ -1,42 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
-import { CreateRentDto, UpdateRentDto } from './dto/request';
-import { RentResponseDto } from './dto/response';
-
 import { RentsService } from './rents.service';
+import { CreateRentDto } from './dto/request';
 
 @Controller()
 export class RentsController {
   constructor(private readonly rentsService: RentsService) {}
 
-  @MessagePattern('rents.find.all')
-  async findAll(): Promise<RentResponseDto[]> {
-    return this.rentsService.findAll();
-  }
-
-  @MessagePattern('rents.find.one')
-  async findOne(@Payload('rentId') rentId: string): Promise<RentResponseDto> {
-    return this.rentsService.findOne(rentId);
-  }
-
-  @MessagePattern('rents.find.by.ids')
-  async findByIds(@Payload() rentsIds: string[]): Promise<RentResponseDto[]> {
-    return this.rentsService.findByIds(rentsIds);
-  }
-
   @MessagePattern('rents.save')
-  async save(@Payload() request: CreateRentDto): Promise<RentResponseDto> {
-    return this.rentsService.save(request);
-  }
-
-  @MessagePattern('rents.update')
-  async update(@Payload() request: UpdateRentDto): Promise<RentResponseDto> {
-    return this.rentsService.update(request);
-  }
-
-  @MessagePattern('rents.remove')
-  async remove(@Payload('rentId') rentId: string): Promise<RentResponseDto> {
-    return this.rentsService.remove(rentId);
+  async save(@Payload() request: CreateRentDto): Promise<void> {
+    this.rentsService.save(request);
   }
 }
