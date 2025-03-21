203d4e543b26afbd20fd125fdc2cd83a54eacf4d
JosueEC <josueev.cruz@gmail.com>
2025-03-20 18:50:38 -0600

    [REFACTOR](rooms-types.repository.interface): Implements by the base-repository interface instead of the repository interface

--- src/rooms-types/repository/interfaces/rooms-types.repository.interface.ts
+++ src/rooms-types/repository/interfaces/rooms-types.repository.interface.ts
@@ -1,6 +1,6 @@
-import { IRepository } from 'src/common/repository';
+import { IBaseRepository } from 'src/common/repository';
 import { CreateRoomTypeRequest } from 'src/grpc/proto/rooms/rooms_types.pb';
 import { RoomType } from 'src/rooms-types/entity/room-type.entity';
 
 export interface IRoomsTypesRepository
-  extends IRepository<RoomType, CreateRoomTypeRequest> {}
+  extends IBaseRepository<RoomType, CreateRoomTypeRequest> {}
