2061838cbd9def5989bc727761676c46684f1ca8
JosueEC <josueev.cruz@gmail.com>
2025-03-20 18:52:01 -0600

    [REFACTOR](reservations-origins.repository.interface): Implements by the base-repository interface instead of the repository interface

--- src/reservations-origins/repository/interfaces/reservations-origins.repository.interface.ts
+++ src/reservations-origins/repository/interfaces/reservations-origins.repository.interface.ts
@@ -1,6 +1,6 @@
-import { IRepository } from 'src/common/repository';
+import { IBaseRepository } from 'src/common/repository';
 import { CreateReservationOriginRequest } from 'src/grpc/proto/rooms/reservations_origins.pb';
 import { ReservationOrigin } from 'src/reservations-origins/entity/reservation-origin.entity';
 
 export interface IReservationsOriginsRepository
-  extends IRepository<ReservationOrigin, CreateReservationOriginRequest> {}
+  extends IBaseRepository<ReservationOrigin, CreateReservationOriginRequest> {}
