83e36ae9fcf9f5c859860e16c4c30ccbc9b1741c
JosueEC <josueev.cruz@gmail.com>
2025-03-20 18:54:10 -0600

    [REFACTOR](rents-extras.module): Export the rents-extras service and import the rates-extras module

--- src/rents-extras/rents-extras.module.ts
+++ src/rents-extras/rents-extras.module.ts
@@ -6,10 +6,12 @@ import { RentExtra } from './entity/rent-extra.entity';
 import { RentsExtrasController } from './rents-extras.controller';
 import { RentsExtrasService } from './rents-extras.service';
 import { RentsExtrasRepository } from './repository/rents-extras.repository';
+import { RatesExtrasModule } from 'src/rates-extras/rates-extras.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RentExtra])],
+  imports: [TypeOrmModule.forFeature([RentExtra]), RatesExtrasModule],
   providers: [RentsExtrasRepository, RentsExtrasService],
   controllers: [RentsExtrasController],
+  exports: [RentsExtrasService],
 })
 export class RentsExtrasModule {}
