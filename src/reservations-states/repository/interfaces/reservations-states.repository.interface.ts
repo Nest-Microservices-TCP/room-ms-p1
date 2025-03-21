21dac689ec58c61c4a7059b35bec3de3cbcdd3be
JosueEC <josueev.cruz@gmail.com>
2025-03-20 18:51:47 -0600

    [REFACTOR](reservations-states.repository.interface): Implements by the base-repository interface instead of the repository interface

--- src/reservations-states/repository/interfaces/reservations-states.repository.interface.ts
+++ src/reservations-states/repository/interfaces/reservations-states.repository.interface.ts
@@ -1,6 +1,6 @@
-import { IRepository } from 'src/common/repository';
+import { IBaseRepository } from 'src/common/repository';
 import { CreateReservationStateRequest } from 'src/grpc/proto/rooms/reservations_states.pb';
 import { ReservationState } from 'src/reservations-states/entity/reservation-state.entity';
 
 export interface IReservationsStatesRepository
-  extends IRepository<ReservationState, CreateReservationStateRequest> {}
+  extends IBaseRepository<ReservationState, CreateReservationStateRequest> {}
