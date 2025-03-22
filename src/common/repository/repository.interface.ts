f573f55b03056d15a69830b021e0cc62dda72b2e
JosueEC <josueev.cruz@gmail.com>
2025-03-21 21:41:36 -0600

    [FEAT](repository.interface): Pass the save-many dto to the interface params

--- src/common/repository/repository.interface.ts
+++ src/common/repository/repository.interface.ts
@@ -1,6 +1,6 @@
 import { IBaseRepository } from './base-repository.interface';
 import { IExtendedRepository } from './extended-repository.interface';
 
-export interface IRepository<T, CreateDto>
+export interface IRepository<T, CreateDto, SaveManyDto>
   extends IBaseRepository<T, CreateDto>,
-    IExtendedRepository<T> {}
+    IExtendedRepository<T, SaveManyDto> {}
