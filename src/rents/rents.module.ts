65921943de0d903cdd66b44a2449b134cb5e6cab
JosueEC <josueev.cruz@gmail.com>
2025-03-21 21:42:33 -0600

    [FEAT](rents.module): Inject the rooms-module and the rents-extras module

--- src/rents/rents.module.ts
+++ src/rents/rents.module.ts
@@ -3,14 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Rent } from './entity';
 
-import { RentsController } from './rents.controller';
 import { RentsService } from './rents.service';
+import { RentsController } from './rents.controller';
 import { RentsRepository } from './repository/rents.repository';
 
 import { RatesModule } from 'src/rates/rates.module';
+import { RoomsModule } from 'src/rooms/rooms.module';
+import { RentsExtrasModule } from 'src/rents-extras/rents-extras.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Rent]), RatesModule],
+  imports: [
+    TypeOrmModule.forFeature([Rent]),
+    RatesModule,
+    RoomsModule,
+    RentsExtrasModule,
+  ],
   controllers: [RentsController],
   providers: [RentsRepository, RentsService],
 })
