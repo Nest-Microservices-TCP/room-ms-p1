f352dece288994a22f9e50aa3aca5fd7285c2150
JosueEC <josueev.cruz@gmail.com>
2025-03-30 18:52:47 -0600

    [FEAT](reservations-origins.service): Add the find-one service method

--- src/reservations-origins/reservations-origins.service.ts
+++ src/reservations-origins/reservations-origins.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { HandleRpcExceptions } from 'src/common/decorators';
 
-import { CreateReservationOriginRequest } from 'src/grpc/proto/rooms/reservations_origins.pb';
+import {
+  CreateReservationOriginRequest,
+  FindOneReservationOriginRequest,
+} from 'src/grpc/proto/rooms/reservations_origins.pb';
 
 import { ReservationsOriginsRepository } from './repository/reservations-origins.repository';
+import { ReservationOrigin } from './entity/reservation-origin.entity';
 
 @Injectable()
 export class ReservationsOriginsService {
@@ -14,5 +18,14 @@ export class ReservationsOriginsService {
   @HandleRpcExceptions()
   save(request: CreateReservationOriginRequest): void {
     this.reservationsOriginsRepository.save(request);
+  }
+
+  @HandleRpcExceptions()
+  findOne(
+    request: FindOneReservationOriginRequest,
+  ): Promise<ReservationOrigin> {
+    const { reservation_origin_id } = request;
+
+    return this.reservationsOriginsRepository.findOne(reservation_origin_id);
   }
 }
