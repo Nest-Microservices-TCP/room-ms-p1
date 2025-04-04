35fbddbaa4e23a62fc8aa5a089abcea896673949
JosueEC <josueev.cruz@gmail.com>
2025-04-03 19:54:35 -0600

    [CONFIG](envs.ts): Validate and expos the rabbitmq rents-queue variable

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -15,6 +15,8 @@ interface EnvVars {
   RABBITMQ_PORT: number;
   RABBITMQ_USER: string;
   RABBITMQ_PASSWORD: string;
+
+  RMQ_RENTS_QUEUE: string;
 }
 
 const envSchema = joi
@@ -32,6 +34,8 @@ const envSchema = joi
     RABBITMQ_PORT: joi.number().required(),
     RABBITMQ_USER: joi.string().required(),
     RABBITMQ_PASSWORD: joi.string().required(),
+
+    RMQ_RENTS_QUEUE: joi.string().required(),
   })
   .unknown(true);
 
@@ -57,4 +61,6 @@ export const envs = {
   rabbitMqPort: envVars.RABBITMQ_PORT,
   rabbitMqUser: envVars.RABBITMQ_USER,
   rabbitMqPassword: envVars.RABBITMQ_PASSWORD,
+
+  rmqRentsQueue: envVars.RMQ_RENTS_QUEUE,
 };
