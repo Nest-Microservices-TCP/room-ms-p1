b9c3e88f6b106eb50a416a864aa94c21b9bdf71b
JosueEC <josueev.cruz@gmail.com>
2025-04-09 21:53:45 -0600

    [FEAT](rooms-types.service): Add the find-one service method

--- src/rooms-types/rooms-types.service.ts
+++ src/rooms-types/rooms-types.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { HandleRpcExceptions } from 'src/common/decorators';
 
-import { CreateRoomTypeRequest } from 'src/grpc/proto-files/rooms/rooms_types.pb';
+import {
+  CreateRoomTypeRequest,
+  FindOneRoomTypeRequest,
+} from 'src/grpc/proto-files/rooms/rooms_types.pb';
 
 import { RoomsTypesRepository } from './repository/rooms-types.repository';
+import { RoomType } from './entity/room-type.entity';
 
 @Injectable()
 export class RoomsTypesService {
@@ -12,5 +16,12 @@ export class RoomsTypesService {
   @HandleRpcExceptions()
   save(request: CreateRoomTypeRequest): void {
     this.roomsTypesRepository.save(request);
+  }
+
+  @HandleRpcExceptions()
+  async findOne(request: FindOneRoomTypeRequest): Promise<RoomType> {
+    const { room_type_id } = request;
+
+    return this.roomsTypesRepository.findOne(room_type_id);
   }
 }
