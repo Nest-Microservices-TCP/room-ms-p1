cd4d8597162fa41d13d02e081285f987b28e4e4a
JosueEC <josueev.cruz@gmail.com>
2025-04-18 22:33:44 -0600

    [CHORE](rooms-types.seed): Add the UUID value for the records

--- src/database/seeds/rooms-types.seed.sql
+++ src/database/seeds/rooms-types.seed.sql
@@ -1,9 +1,20 @@
-INSERT INTO rooms_types ("name", included_people)
+INSERT INTO public.rooms
+(created_at, updated_at, created_by, updated_by, status, deleted_at, room_id, "number", room_state_id, room_type_id)
 VALUES
-('Pool Villa', 2),
-('Handicap', 2),
-('Pool & Spa Suite', 2),
-('Junior Suite', 2),
-('Sky Suite', 2),
-('Master Suite', 2),
-('Junior Villa', 2);
+('037a118e-1c57-4c81-9306-fda2ec48c7c5'::uuid, 101, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, 'f2c13007-7b6b-4120-95fc-7430119cc3b1'::uuid),
+('f3fa4c27-08a6-4b57-b6ad-1ee5ff6842a7'::uuid, 102, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, 'f2c13007-7b6b-4120-95fc-7430119cc3b1'::uuid),
+('6f29f156-c686-4467-acfe-37e421e76e68'::uuid, 103, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, 'f2c13007-7b6b-4120-95fc-7430119cc3b1'::uuid),
+('7654ceeb-a1b6-464c-97e8-a51a1ca25aa6'::uuid, 104, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, 'f2c13007-7b6b-4120-95fc-7430119cc3b1'::uuid),
+('7dad6375-d8e4-4fde-be93-ee6af7ac3901'::uuid, 105, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '9ab4681d-873a-40ac-bef9-a19fab591649'::uuid),
+('83baeea6-6c8a-4478-b4f4-b14be105f84f'::uuid, 106, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '9ab4681d-873a-40ac-bef9-a19fab591649'::uuid),
+('fb9eb1d4-fd57-4e96-813f-aa610881a420'::uuid, 107, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, 'a2e6a4ac-ebad-4d74-ace1-7d47c360846d'::uuid),
+('59e4dcb3-ccd4-448b-88f0-5f893f8b56fa'::uuid, 108, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, 'a2e6a4ac-ebad-4d74-ace1-7d47c360846d'::uuid),
+('5ebfca99-7595-4169-8859-60318c7ca233'::uuid, 109, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '7186eccb-2b2c-4f09-b6fa-43dfdeddf08f'::uuid),
+('55fe15ac-b6a9-46fd-a3e3-fa823254b090'::uuid, 110, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '7186eccb-2b2c-4f09-b6fa-43dfdeddf08f'::uuid),
+('7340518d-f1e1-445c-b426-ae32ea6f5475'::uuid, 111, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '7186eccb-2b2c-4f09-b6fa-43dfdeddf08f'::uuid),
+('3b08031d-b193-45b2-942a-48177fc834a6'::uuid, 112, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '7186eccb-2b2c-4f09-b6fa-43dfdeddf08f'::uuid),
+('7b57a9eb-bf83-4799-84b8-be5e84cc42e9'::uuid, 113, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '7186eccb-2b2c-4f09-b6fa-43dfdeddf08f'::uuid),
+('a6c64b2a-6dc0-469c-a75e-546392642dce'::uuid, 114, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '299cba9b-67ff-4992-a55e-a299c89ccdab'::uuid),
+('5d8446fc-0e9c-42d5-bbaf-906deb37bc0e'::uuid, 115, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '299cba9b-67ff-4992-a55e-a299c89ccdab'::uuid),
+('d2f82a95-4ce8-46db-865f-94b0b8bfd1da'::uuid, 116, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '299cba9b-67ff-4992-a55e-a299c89ccdab'::uuid),
+('b1157b20-2335-40c2-9876-488800fa1254'::uuid, 117, 'faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, '299cba9b-67ff-4992-a55e-a299c89ccdab'::uuid);
