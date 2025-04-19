d49071517c7fb3166f54017045295d031556b4bf
JosueEC <josueev.cruz@gmail.com>
2025-04-18 22:33:31 -0600

    [CHORE](rooms-states.seed): Add the UUID value for the records

--- src/database/seeds/rooms-states.seed.sql
+++ src/database/seeds/rooms-states.seed.sql
@@ -1,14 +1,15 @@
-INSERT INTO public.rooms_states (status, "name")
+INSERT INTO public.rooms_states
+(created_at, updated_at, created_by, updated_by, status, deleted_at, room_state_id, "name", description)
 VALUES
-('Preparada'),
-('Sucia'),
-('A la venta'),
-('Ocupada'),
-('Supervisión'),
-('Mantenimiento'),
-('Limpieza'),
-('Supervisión pendiente'),
-('Supervisión en curso'),
-('Reservada'),
-('Bloqueada'),
-('Room service');
+('7f161ee3-3efd-4460-98b2-d607f1d36f9e'::uuid, 'Preparada', NULL),
+('7b2120bd-2db0-4767-9647-febdee886066'::uuid, 'Sucia', NULL),
+('faf6f33d-a3c7-422a-8333-4e5ba216cd70'::uuid, 'A la venta', NULL),
+('f72e0216-6f48-4c29-92c6-c4898a00c47f'::uuid, 'Ocupada', NULL),
+('a4d3d952-3162-45ef-b4d9-fc2e22c2fbec'::uuid, 'Supervisión', NULL),
+('23c8d0e7-ef58-4179-98c4-0ef0f508e449'::uuid, 'Mantenimiento', NULL),
+('70876791-dd8b-4775-b8f1-95b4e39ce043'::uuid, 'Limpieza', NULL),
+('5215a2d2-78d8-48e6-b1b1-9901d6bd6f4b'::uuid, 'Supervisión pendiente', NULL),
+('609edebe-61c1-4f07-9642-af33e940945c'::uuid, 'Supervisión en curso', NULL),
+('d508d200-10e8-4d1e-8301-03485d4d3449'::uuid, 'Reservada', NULL),
+('c5501c2c-d199-46b1-b59b-a73fa3448c6f'::uuid, 'Bloqueada', NULL),
+('fd1e59a8-a4fa-4c04-b156-ea02971dd02c'::uuid, 'Room service', NULL);
