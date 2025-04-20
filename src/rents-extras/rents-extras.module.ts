a8456a2f2dcd237cd9473f182256d669f03c2732
JosueEC <josueev.cruz@gmail.com>
2025-04-19 19:40:27 -0600

    [CHORE](rents-extras.module): Sort and group the imports sentences

--- src/rents-extras/rents-extras.module.ts
+++ src/rents-extras/rents-extras.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { RentExtra } from './entity/rent-extra.entity';
 
-import { RentsExtrasController } from './rents-extras.controller';
 import { RentsExtrasService } from './rents-extras.service';
+import { RentsExtrasController } from './rents-extras.controller';
 import { RentsExtrasRepository } from './repository/rents-extras.repository';
+
 import { RatesExtrasModule } from 'src/rates-extras/rates-extras.module';
 
 @Module({
