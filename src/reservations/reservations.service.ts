5f6aac10e6017f9ebc1cdd595f91c196377656ba
JosueEC <josueev.cruz@gmail.com>
2025-04-25 23:15:27 -0600

    [CHORE](reservations.service): Add the find-one service method

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -5,8 +5,11 @@ import { ReservationsRepository } from './repository/reservations.repository';
 
 import {
   ReservationState,
+  ReservationResponse,
   CreateReservationRequest,
+  FindOneReservationRequest,
 } from 'src/grpc/rooms/reservations.pb';
+import { Currency } from 'src/grpc/common/common_enums.pb';
 import { SaveReservationType } from './types';
 
 @Injectable()
@@ -27,5 +30,34 @@ export class ReservationsService {
     };
 
     this.reservationsRepository.save(newReservation);
+  }
+
+  async findOne(
+    request: FindOneReservationRequest,
+  ): Promise<ReservationResponse> {
+    const { reservation_id } = request;
+
+    const reservation =
+      await this.reservationsRepository.findOne(reservation_id);
+
+    const {
+      total_cents,
+      total_currency,
+      outstanding_balance_cents,
+      outstanding_balance_currency,
+      ...rest
+    } = reservation;
+
+    return {
+      ...rest,
+      total: {
+        amount_cents: total_cents,
+        currency: Currency[total_currency],
+      },
+      outstanding_balance: {
+        amount_cents: outstanding_balance_cents,
+        currency: Currency[outstanding_balance_currency],
+      },
+    };
   }
 }
