577406bc2f1af3ece338f5189af3ee225a8d4b61
JosueEC <josueev.cruz@gmail.com>
2025-04-29 23:22:03 -0600

    [CHORE](typeorm-exception.filter): Map the code and message details rpc error

--- src/common/exceptions/filters/typeorm-exceptions.filter.ts
+++ src/common/exceptions/filters/typeorm-exceptions.filter.ts
@@ -1,13 +1,28 @@
+import { throwError } from 'rxjs';
+import { status } from '@grpc/grpc-js';
 import { QueryFailedError } from 'typeorm';
 import { RpcException } from '@nestjs/microservices';
 import { Catch, ExceptionFilter } from '@nestjs/common';
+import { CustomExceptionDetails } from '../interfaces';
 
 @Catch(QueryFailedError)
 export class TypeORMExceptionsFilter implements ExceptionFilter {
-  catch() {
-    throw new RpcException({
-      message: 'Database error',
-      status: 500,
-    });
+  catch(exception: QueryFailedError) {
+    const details: CustomExceptionDetails = {
+      details: exception?.message || 'Unexpected error',
+      cause: 'Esta es la causa',
+      metadata: { service: 'Un servicio' },
+    };
+
+    const code = status.INTERNAL;
+    const message = JSON.stringify(details);
+
+    return throwError(
+      () =>
+        new RpcException({
+          message,
+          code,
+        }),
+    );
   }
 }
