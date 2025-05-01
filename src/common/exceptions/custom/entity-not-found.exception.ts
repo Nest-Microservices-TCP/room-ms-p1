4ad4e59cad0494173f049aefa7abeba6a12e6bfc
JosueEC <josueev.cruz@gmail.com>
2025-04-30 22:12:00 -0600

    [CHORE](entity-not-found.exception): Use the get-caller-info function

--- src/common/exceptions/custom/entity-not-found.exception.ts
+++ src/common/exceptions/custom/entity-not-found.exception.ts
@@ -1,7 +1,12 @@
 import { NotFoundException } from '@nestjs/common';
+import { getCallerInfo } from 'src/common/utils';
 
 export class EntityNotFoundException extends NotFoundException {
   constructor(entityName: string) {
-    super(`The ${entityName} with provided ID does not exist`);
+    const { className, methodName } = getCallerInfo();
+
+    const message = `The ${entityName} with provided ID does not exist`;
+
+    super({ className, methodName, message });
   }
 }
