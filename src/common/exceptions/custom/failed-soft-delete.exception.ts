5d174e49c920f7045e60018e0c7a7f2f2e2b9ec3
JosueEC <josueev.cruz@gmail.com>
2025-04-30 22:13:52 -0600

    [CHORE](failed-soft-delete.exception): Use the get-caller-info function

--- src/common/exceptions/custom/failed-soft-delete.exception.ts
+++ src/common/exceptions/custom/failed-soft-delete.exception.ts
@@ -1,7 +1,12 @@
 import { InternalServerErrorException } from '@nestjs/common';
+import { getCallerInfo } from 'src/common/utils';
 
 export class FailedSoftDeleteException extends InternalServerErrorException {
   constructor(entityName: string) {
-    super(`Error to soft delete the ${entityName}, try again`);
+    const { className, methodName } = getCallerInfo();
+
+    const message = `Error to soft delete the ${entityName}, try again`;
+
+    super({ className, methodName, message });
   }
 }
