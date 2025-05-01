d4e3d6fbd8cc7b41db3ba22118d9d7418ca61fbc
JosueEC <josueev.cruz@gmail.com>
2025-04-30 22:18:41 -0600

    [CHORE](rates.service): Remove the handle-rpc-exception decorator from the service methods

--- src/rates/rates.service.ts
+++ src/rates/rates.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { HandleRpcExceptions } from 'src/common/decorators';
 
 import {
   FindRatesResponse,
@@ -16,26 +15,22 @@ import { Rate } from './entity/rate.entity';
 export class RatesService {
   constructor(private readonly ratesRepository: RatesRepository) {}
 
-  @HandleRpcExceptions()
   save(request: CreateRateRequest): void {
     this.ratesRepository.save(request);
   }
 
-  @HandleRpcExceptions()
   async findOne(request: FindOneRateRequest): Promise<Rate> {
     const { rate_id } = request;
 
     return await this.ratesRepository.findOne(rate_id);
   }
 
-  @HandleRpcExceptions()
   async find(): Promise<FindRatesResponse> {
     const rates = await this.ratesRepository.find();
 
     return { rates };
   }
 
-  @HandleRpcExceptions()
   async findByIds(request: FindRatesByIdsRequest): Promise<FindRatesResponse> {
     const { rates_ids } = request;
 
