c4df5424b23cd681e0d8b622a1b0927e2f3f776b
JosueEC <josueev.cruz@gmail.com>
2025-05-01 21:00:35 -0600

    [CHORE](common/exceptions/interfaces): Delete the MetadataException and the CustomExceptionDetails interfaces

--- src/common/exceptions/interfaces/custom-exception-details.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface MetadataException {
-  className: string;
-  methodName: string;
-}
-
-export interface CustomExceptionDetails {
-  exception_message: string;
-  metadata?: MetadataException;
-}
--- src/common/exceptions/interfaces/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './custom-exception-details.interface';
