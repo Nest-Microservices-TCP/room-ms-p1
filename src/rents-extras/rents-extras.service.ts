3d79cdd55f52aa5707788b1c6f931a0e3e5d098b
JosueEC <josueev.cruz@gmail.com>
2025-05-20 21:49:39 -0600

    [CHORE](rents-extras.service): Define the bulk-save an the create-many service methods

--- src/rents-extras/rents-extras.service.ts
+++ src/rents-extras/rents-extras.service.ts
@@ -1,22 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
-
 import { HandleRpcExceptions } from 'src/common/decorators';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateRentExtraDto, UpdateRentExtraDto } from './dto/request';
+import {
+  SaveRentExtraDto,
+  UpdateRentExtraDto,
+  CreateRentExtraDto,
+  SaveManyRentsExtrasDto,
+} from './dto/request';
 import { RentExtraResponseDto } from './dto/response';
+import { DeleteResultResponse } from 'src/common/dto/response';
 
+import { RatesExtrasService } from 'src/rates-extras/rates-extras.service';
 import { RentsExtrasRepository } from './repository/rents-extras.repository';
+
+import { RentExtra } from './entity/rent-extra.entity';
 
 @Injectable()
 export class RentsExtrasService {
-  constructor(private readonly rentsExtrasRepository: RentsExtrasRepository) {}
+  constructor(
+    private readonly rentsExtrasRepository: RentsExtrasRepository,
+    private readonly ratesExtrasService: RatesExtrasService,
+  ) {}
 
   private plainToInstanceDto(data: unknown): any {
     return plainToInstance(RentExtraResponseDto, data, {
       excludeExtraneousValues: true,
     });
+  }
+
+  async createMany(request: {
+    rents_extras: CreateRentExtraDto[];
+    rate_id: string;
+  }) {
+    const { rate_id, rents_extras } = request;
+
+    if (rents_extras.length === 0) return;
+
+    const extrasIds = rents_extras.map((rentExtra) => rentExtra.extra_id);
+
+    const rateExtras = await this.ratesExtrasService.findByRate({
+      extras_ids: extrasIds,
+      rate_id,
+    });
+
+    console.log({ rateExtras });
   }
 
   @HandleRpcExceptions()
@@ -42,10 +70,15 @@ export class RentsExtrasService {
   }
 
   @HandleRpcExceptions()
-  async save(request: CreateRentExtraDto): Promise<RentExtraResponseDto> {
+  async save(request: SaveRentExtraDto): Promise<RentExtraResponseDto> {
     const newRentExtra = await this.rentsExtrasRepository.save(request);
 
     return this.plainToInstanceDto(newRentExtra);
+  }
+
+  @HandleRpcExceptions()
+  async bulkSave(request: SaveManyRentsExtrasDto): Promise<RentExtra[]> {
+    return this.rentsExtrasRepository.bulkSave(request);
   }
 
   @HandleRpcExceptions()
